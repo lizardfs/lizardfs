b1fa0b0292963b552167b4dbe45f3ee7c42716d9
jbogusz <jbogusz@57e86ddf-214e-0410-857f-c6200d701b87>
2008-07-08 09:07:52 +0000

    - support pkgconfig in /opt/local/share/aclocal on MacOS X

--- autogen.sh
+++ autogen.sh
@@ -2,7 +2,14 @@
 
 set -e
 
-aclocal
+# MacOS
+if [ -d /opt/local/share/aclocal ]; then
+	MACROSINC=-I/opt/local/share/aclocal
+else
+	MACROSINC=
+fi
+
+aclocal $MACROSINC
 autoconf
 autoheader
 automake -a -c
