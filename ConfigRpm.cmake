727444a2796f3b6bee7db7d0eb89b0435dbcbe77
camelon <kamil.pachulski@skytechnology.pl>
2013-08-28 18:28:00 +0200

    Add switches for CPack generators

---
+++ ConfigRpm.cmake
@@ -0,0 +1,6 @@
+set(CPACK_RPM_PACKAGE_GROUP "System Environment/Daemons")
+set(CPACK_RPM_PACKAGE_LICENSE "GPL v3")
+set(CPACK_RPM_PACKAGE_REQUIRES "fuse-devel pkgconfig python zlib-devel")
+
+set(CPACK_RPM_COMPONENT_INSTALL TRUE)
+
