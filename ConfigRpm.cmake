d383f6a561500bd0edbeffc8046806c83cd6ad94
pjanicki <pawel.janicki@skytechnology.pl>
2013-09-01 14:44:11 +0200

    Build master.rpm by CPack

--- ConfigRpm.cmake
+++
@@ -1,6 +0,0 @@
-set(CPACK_RPM_PACKAGE_GROUP "System Environment/Daemons")
-set(CPACK_RPM_PACKAGE_LICENSE "GPL v3")
-set(CPACK_RPM_PACKAGE_REQUIRES "fuse-devel pkgconfig python zlib-devel")
-
-set(CPACK_RPM_COMPONENT_INSTALL TRUE)
-
