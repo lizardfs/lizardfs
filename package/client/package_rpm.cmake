8479c70ebd47001ffc2a680f22c9605258780659
pjanicki <pawel.janicki@skytechnology.pl>
2013-09-03 16:05:26 +0200

    Fix RPM scripts not executed. Fix bad comonent name in common postinst

--- package/client/package_rpm.cmake
+++ package/client/package_rpm.cmake
@@ -2,4 +2,4 @@ set(COMPONENT_NAME "chunkserver")
 
 set(CPACK_${COMPONENT_NAME}_PACKAGE_DESCRIPTION_SUMMARY "LizardFS client")
 
-set(CPACK_RPM_${COMPONENT_NAME}_PACKAGE_REQUIRES "filesystem fuse lizardfs-common" PARENT_SCOPE)
+set(CPACK_RPM_${COMPONENT_NAME}_PACKAGE_REQUIRES "filesystem fuse lizardfs-common")
