74856cc5c8bb2cbbc86d3e17962bd3bb73cd2c8d
witekaj <zaxon72@gmail.com>
2013-09-05 20:17:02 +0200

    Add thread synchronization and some tiny changes

--- mfscommon/crc_sse4.cc
+++ mfscommon/crc_sse4.cc
@@ -1,22 +1,46 @@
+/* 2013/09/31 witekj@op.pl
+ *
+*/
+
 #include <crc_sse4.h>
 
-CrcContainer::CRC_BAG_TYPE CrcContainer::m_CrcBag;
+pthread_mutex_t CrcPool::m_CrcPoolLock = PTHREAD_MUTEX_INITIALIZER;
 
-crcutil_interface::CRC* CrcContainer::createInstance( const CRC_UINT64& pPoly ){
+CrcPool::CRC_BAG_TYPE CrcPool::m_CrcBag;
+
+crcutil_interface::CRC* CrcPool::createInstance( const CRC_UINT64& pPoly ){
     bool isSse = crcutil_interface::CRC::IsSSE42Available();
     crcutil_interface::CRC* crc = crcutil_interface::CRC::Create(
                                   pPoly, 0, 32, true, 0, 0, 0,
                                   isSse , NULL );
-	CrcContainer::m_CrcBag.insert( make_pair( make_pair( getpid(), pPoly ) , crc ) );
+    CrcPool::m_CrcBag.insert( make_pair( make_pair( pthread_self(), pPoly ) , crc ) );
 
     return crc;
 }
 
+#define CRC_POOL_LOCK \
+if ( pthread_mutex_lock( &m_CrcPoolLock ) < 0 ) { \
+    printf( "lock error: %s\n", __FUNCTION__ ); \
+    return NULL; \
+}
 
-crcutil_interface::CRC* CrcContainer::getCrc( const CRC_UINT64& pPoly ) {
-    CRC_BAG_TYPE::iterator iter = CrcContainer::m_CrcBag.find( make_pair( getpid(), pPoly )  );
-    if( iter != CrcContainer::m_CrcBag.end() ) {
+
+#define CRC_POOL_UNLOCK \
+if ( pthread_mutex_unlock( &m_CrcPoolLock ) < 0 ) { \
+    printf("unlock error: %s\n", __FUNCTION__ ); \
+}
+
+crcutil_interface::CRC* CrcPool::getCrc( const CRC_UINT64& pPoly ) {
+    CRC_POOL_LOCK
+    CRC_BAG_TYPE::iterator iter = CrcPool::m_CrcBag.find( make_pair( pthread_self(), pPoly )  );
+    if( iter != CrcPool::m_CrcBag.end() ) {
+        CRC_POOL_UNLOCK
         return iter->second;
     }
-    return createInstance( pPoly );
+
+    crcutil_interface::CRC* crc = createInstance( pPoly );
+    CRC_POOL_UNLOCK
+    return crc;
 }
+
+
--- mfscommon/crc_sse4.h
+++ mfscommon/crc_sse4.h
@@ -5,6 +5,7 @@
 #ifndef  _CRC_SSE4__H__
 #define  _CRC_SSE4__H__
 
+#include <pthread.h>
 #include <inttypes.h>
 #include <unistd.h>
 #include <utility>
@@ -16,18 +17,19 @@ using namespace std;
 
 typedef crcutil_interface::UINT64 CRC_UINT64;
 
-const CRC_UINT64 INTEL_CRC = 0x82F63B78;
+const CRC_UINT64 CRC_INTEL = 0x82F63B78;
 
 
-class CrcContainer
+class CrcPool
 {
 public:
-	typedef map< pair< __pid_t, CRC_UINT64 >, crcutil_interface::CRC* > CRC_BAG_TYPE;
+    typedef map< pair< pthread_t, CRC_UINT64 >, crcutil_interface::CRC* > CRC_BAG_TYPE;
     static crcutil_interface::CRC* getCrc( const CRC_UINT64& pPoly );
 
 private:
-	 static CRC_BAG_TYPE m_CrcBag;
-	 static crcutil_interface::CRC* createInstance( const CRC_UINT64& pPoly );
+    static pthread_mutex_t m_CrcPoolLock;
+    static CRC_BAG_TYPE m_CrcBag;
+    static crcutil_interface::CRC* createInstance( const CRC_UINT64& pPoly );
 };
 
 
@@ -37,7 +39,7 @@ class LizardCrc
     public:
 		static uint32_t mycrc32( uint32_t crc, const uint8_t* block,uint32_t leng )
 		{
-			crcutil_interface::CRC* crcCalc = CrcContainer::getCrc( T );
+            crcutil_interface::CRC* crcCalc = CrcPool::getCrc( T );
 		    CRC_UINT64 lo = 0;
 
 			if( 0 == crc )  {
@@ -54,7 +56,7 @@ class LizardCrc
 
         static uint32_t mycrc32_combine( uint32_t crc1, uint32_t crc2, uint32_t leng2 ) 
 		{
-			crcutil_interface::CRC* crcCalc = CrcContainer::getCrc( T );
+            crcutil_interface::CRC* crcCalc = CrcPool::getCrc( T );
 			CRC_UINT64 crcTmp1 = static_cast< CRC_UINT64 >( crc1 );
 			crcCalc->Concatenate( static_cast< CRC_UINT64 >( crc2 ), 0, static_cast< CRC_UINT64 >( leng2 ), &crcTmp1 );
 			return static_cast< uint32_t >( crcTmp1 );
