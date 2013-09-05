9a7c1accfc5af11b563d61c6897fe433865a7fea
pjanicki <pawel.janicki@skytechnology.pl>
2013-09-05 07:53:11 +0200

    Build: Update DEB packaging parameters

--- package/client/package_deb.cmake
+++ package/client/package_deb.cmake
@@ -1,5 +1,8 @@
 set(COMPONENT_NAME "client")
 
+set(CPACK_DEB_${COMPONENT_NAME}_PACKAGE_DEPENDS "fuse")
+set(CPACK_DEB_${COMPONENT_NAME}_PACKAGE_DESCRIPTION "LizardFS fuse client")
+
 configure_file(empty.prerm.in deb.${COMPONENT_NAME}.prerm)
 configure_file(empty.postinst.in deb.${COMPONENT_NAME}.postinst)
 set_deb_component_control_extra(${COMPONENT_NAME} "postinst" "${CMAKE_CURRENT_BINARY_DIR}/deb.${COMPONENT_NAME}.postinst")
