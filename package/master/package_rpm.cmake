f85213d841eef0a20d5a3eb1a573a59484304025
pjanicki <pawel.janicki@skytechnology.pl>
2013-09-09 02:16:37 -0400

    Package: Fix rpm build to produce installable packages

--- package/master/package_rpm.cmake
+++ package/master/package_rpm.cmake
@@ -2,12 +2,15 @@ set(COMPONENT_NAME "master")
 
 set(CPACK_RPM_${COMPONENT_NAME}_PACKAGE_REQUIRES "filesystem lizardfs-common zlib")
 
-configure_file(daemon.prerm.in rpm.${COMPONENT_NAME}.prerm)
-configure_file(daemon.postinst.in rpm.${COMPONENT_NAME}.postinst)
+configure_file(daemon.rpm.prerm.in rpm.${COMPONENT_NAME}.prerm.in)
+configure_file(daemon.rpm.postinst.in rpm.${COMPONENT_NAME}.postinst.in)
+configure_file(${CMAKE_CURRENT_BINARY_DIR}/rpm.${COMPONENT_NAME}.postinst.in rpm.${COMPONENT_NAME}.postinst)
+configure_file(${CMAKE_CURRENT_BINARY_DIR}/rpm.${COMPONENT_NAME}.prerm.in rpm.${COMPONENT_NAME}.prerm)
 set(CPACK_RPM_${COMPONENT_NAME}_POST_INSTALL_SCRIPT_FILE ${CMAKE_CURRENT_BINARY_DIR}/rpm.${COMPONENT_NAME}.postinst)
 set(CPACK_RPM_${COMPONENT_NAME}_PRE_UNINSTALL_SCRIPT_FILE ${CMAKE_CURRENT_BINARY_DIR}/rpm.${COMPONENT_NAME}.prerm)
 
 set(CPACK_RPM_${COMPONENT_NAME}_USER_FILELIST
+    "%ignore /etc/init.d" #Provided by chkconfig
     "%ignore /usr"        #Provided by filesystem
     "%ignore /usr/sbin"
     "%ignore ${DATA_PATH}" #Provided by lizardfs-common
