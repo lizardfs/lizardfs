5dfa5bd3e0a9719898651c5ef9875b7ac8736d60
Marcin Sulikowski <marcin.sulikowski@skytechnology.pl>
2013-11-21 11:59:44 +0100

    tests: Add different goals (eg. xor) to test_data_integrity
    This commit enhances test_data_integrity to test different goals,
    ie. goal 1, 2 and xor2.
    
    Change-Id: Iea197a716f576fd1f98f524e91d4c5ca3c8d62c5

--- tests/test_suites/SystemTestingSuite/test_data_integrity.sh
+++ tests/test_suites/SystemTestingSuite/test_data_integrity.sh
@@ -1,10 +1,17 @@
 timeout_set 1 hour
 
-CHUNKSERVERS=2 \
+CHUNKSERVERS=3 \
 	MOUNT_EXTRA_CONFIG="mfscachemode=NEVER" \
 	setup_local_empty_lizardfs info
 
 cd ${info[mount0]}
+directories=""
+for goal in 1 2 xor2; do
+	dir="goal_$goal"
+	mkdir "$dir"
+	mfssetgoal "$goal" "$dir"
+	directories="$directories $dir"
+done
 
 test_worker() {
 	local min_size=$1
@@ -12,16 +19,16 @@ test_worker() {
 	while ! test_frozen; do
 		local block_size=$(random 1024 128K)
 		local file_size=$(random $min_size $max_size)
-		local file=$(unique_file)
+		local file="$(shuf -n1 -e $directories)/$(unique_file)"
 		echo FILE_SIZE=$file_size BLOCK_SIZE=$block_size file-generate "$file"
 		if FILE_SIZE=$file_size BLOCK_SIZE=$block_size file-generate "$file"; then
 			if ! file-validate "$file"; then
-				test_add_failure "Invalid data: block $block_size, size $file_size"
+				test_add_failure "Invalid data: block $block_size, size $file_size, file $file"
 			fi
 			mfssettrashtime 0 "$file" &>/dev/null
 			rm -f "$file"
 		else
-			test_add_failure "file-validate failed: block $block_size, size $file_size"
+			test_add_failure "file-validate failed: block $block_size, size $file_size, file $file"
 		fi
 	done
 }
@@ -33,7 +40,7 @@ test_worker 8  5M  &
 test_worker 1M 50M &
 test_worker 1M 1G  &
 test_worker 1G 6G  &
-sleep 2700
+sleep 45m
 
 # Stop workers by killing
 test_freeze_result
