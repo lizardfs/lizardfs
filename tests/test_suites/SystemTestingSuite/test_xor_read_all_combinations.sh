6fdcaa2fc5c614686ca889397d177bc0d130cca8
Marcin Sulikowski <marcin.sulikowski@skytechnology.pl>
2013-11-28 14:31:56 +0100

    tests: Fix test_xor_read_all_combinations
    This test was failing, because it was trying to use non-exiting file
    with the test code.
    
    Change-Id: I863a5bf8c5bc2c65f119ffb70226c6edff3853f4

--- tests/test_suites/SystemTestingSuite/test_xor_read_all_combinations.sh
+++ tests/test_suites/SystemTestingSuite/test_xor_read_all_combinations.sh
@@ -1,7 +1,21 @@
 timeout_set 10 minutes
 
-XOR_LEVEL=9
-READ_MODE=all_combinations
-FILE_SIZE=876M
+CHUNKSERVERS=10 \
+	DISK_PER_CHUNKSERVER=1 \
+	MOUNT_EXTRA_CONFIG="mfscachemode=NEVER" \
+	USE_RAMDISK=YES \
+	setup_local_empty_lizardfs info
 
-source test_suites/TestTemplates/test_xor_read.inc
+dir="${info[mount0]}/dir"
+mkdir "$dir"
+mfssetgoal xor9 "$dir"
+FILE_SIZE=876M file-generate "$dir/file"
+
+for i in {0..9}; do
+	mfschunkserver -c "${info[chunkserver${i}_config]}" stop
+	if ! file-validate "$dir/file"; then
+		test_add_failure "Data read from file without chunkserver $i is different than written"
+	fi
+	mfschunkserver -c "${info[chunkserver${i}_config]}" start
+	sleep 2
+done
