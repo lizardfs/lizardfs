d3b63c11ac7793ae399d6ed9a0f36a571f3772ab
Marcin Sulikowski <marcin.sulikowski@skytechnology.pl>
2013-12-03 13:46:40 +0100

    tests: Test different goals in SystemTestingSuite
    This commit modifies test_dbench and test_compilation_and_rsync
    not to use the ordinary goals only, but also xor2.
    
    Change-Id: I89cf17e22453fd706224a824bb7d6005ee84d937

--- tests/test_suites/SystemTestingSuite/test_dbench.sh
+++ tests/test_suites/SystemTestingSuite/test_dbench.sh
@@ -1,7 +1,7 @@
 timeout_set 1 hour
 assert_program_installed dbench
 
-# Runs dbench for half an hour with 6 clients
+# Runs dbench for half an hour with 5 clients
 dbench_tester() {
 	local dir="$1"
 	cd "$dir"
@@ -14,7 +14,7 @@ CHUNKSERVERS=3 \
 	setup_local_empty_lizardfs info
 
 cd "${info[mount0]}"
-for goal in 1 2; do
+for goal in 1 2 xor2; do
 	mkdir "goal_$goal"
 	mfssetgoal "$goal" "goal_$goal"
 	dbench_tester "goal_$goal" &
