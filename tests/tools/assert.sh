435a115aa647e5a0e87706a60d217c7b5c6255ba
Marcin Sulikowski <sulik@lizardfs.org>
2013-12-05 14:15:38 +0100

    tests: Add assertlocal assertions
    This commit adds a new type of assertion: assertlocal_<condition>. This
    is useful in subshells, when we want to kill the subshell without stopping
    the whole test.

--- tests/tools/assert.sh
+++ tests/tools/assert.sh
@@ -1,4 +1,9 @@
-# (assert|expect)_program_installed <program>
+# Assertion types:
+# * expect_some_condition -- adds error to the test results, but continues the test
+# * assert_some_condition -- adds error to the test results and immediately stops the test
+# * assertlocal_some_condition -- adds error to the results and exits current subshell 
+ 
+# (assert|assertlocal|expect)_program_installed <program>
 assert_template_program_installed_() {
 	local program=$1
 	if ! is_program_installed "$program"; then
@@ -6,7 +11,7 @@ assert_template_program_installed_() {
 	fi
 }
 
-# (assert|expect)_files_equal <file1> <file2>
+# (assert|assertlocal|expect)_files_equal <file1> <file2>
 assert_template_files_equal_() {
 	local file1=$1
 	local file2=$2
@@ -15,7 +20,7 @@ assert_template_files_equal_() {
 	fi
 }
 
-# (assert|expect)_success <command> [<args>...]
+# (assert|assertlocal|expect)_success <command> [<args>...]
 assert_template_success_() {
 	if ! "$@"; then
 		$FAIL_FUNCTION "Command '$*' failed"
@@ -37,13 +42,18 @@ do_assert_failed_() {
 	test_fail $(add_custom_error_message_ "$*")
 }
 
+do_assertlocal_failed_() {
+	test_add_failure $(add_custom_error_message_ "$*")
+	exit 1
+}
+
 do_expect_failed_() {
 	test_add_failure $(add_custom_error_message_ "$*")
 }
 
 # Create expect/assert functions for all templates defined above
 for template in $(typeset -F | grep -o 'assert_template_.*_'); do
-	for type in assert expect; do
+	for type in assert assertlocal expect; do
 		function_name=$(echo $template | sed -re "s/assert_template_(.*)_/${type}_\1"/)
 		eval "$function_name() { FAIL_FUNCTION=do_${type}_failed_ $template \"\$@\" ; }"
 	done
