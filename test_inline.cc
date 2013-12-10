65ec15e922b69c4694f0bc557fd66eaa312cb816
Marcin Sulikowski <sulik@lizardfs.org>
2013-12-10 11:38:06 +0100

    cmake: Remove unused inline test
    The test_inline.cc file was not used and had a common prefix
    with the tests/ directory making it harder to use tab completion
    when navigating through our sources.

--- test_inline.cc
+++
@@ -1,8 +0,0 @@
-/* Test source lifted from /usr/share/autoconf/autoconf/c.m4
-
-   brought from: http://www.cmake.org/Wiki/CMakeTestInline */
-typedef int foo_t;
-static inline foo_t static_foo(){return 0;}
-foo_t foo(){return 0;}
-int main(int argc, char *argv[]){return 0;}
-
