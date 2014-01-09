c2553b4aa4a1157e28a7105dfe3adc46b2652e32
Kamil Pachulski <kamil.pachulski@skytechnology.pl>
2014-01-09 11:37:24 +0100

    tests: Rename and improve chunk conversion test
    test_chunk_replication was actually testing chunk conversions.
    This commit renames this test to test_chunk_type_conversion and improves it.
    
    Change-Id: I25f205b530c60e4025c6342dfc28220cb94f201f

--- tests/test_suites/SanityChecks/test_chunk_replication.sh
+++
@@ -1,8 +0,0 @@
-timeout_set 20 seconds
-WAIT_FOR_REPLICATION=15
-
-NUMBER_OF_CHUNKSERVERS=3
-GOALS_TO_BE_TESTED="1 2 3"
-VERIFY_FILE_CONTENT=NO
-
-source test_suites/TestTemplates/test_chunk_replication.inc
--- tests/test_suites/SystemTestingSuite/test_chunk_replication.sh
+++
@@ -1,8 +0,0 @@
-timeout_set 120 seconds
-WAIT_FOR_REPLICATION=90
-
-NUMBER_OF_CHUNKSERVERS=7
-GOALS_TO_BE_TESTED="2 4 xor2 xor3"
-VERIFY_FILE_CONTENT=YES
-
-source test_suites/TestTemplates/test_chunk_replication.inc
