7018aeb857f7f569a38bbe875f1debd7ed693ecf
Kamil Pachulski <kamil.pachulski@skytechnology.pl>
2014-01-29 19:14:11 +0100

    tests: Add different goals to benchmarks
    Change-Id: Idc485fef7b6655ac4ae3b6a7c9c8423bf5434586

--- tests/test_suites/Benchmarks/test_disk_speed.sh
+++ tests/test_suites/Benchmarks/test_disk_speed.sh
@@ -1,36 +1,38 @@
-timeout_set 6 minutes
+timeout_set 30 minutes
 
 CHUNKSERVERS=3 \
 	setup_local_empty_lizardfs info
 
+file_size_mb=1000
 cd "${info[mount0]}"
 
-# file size in MB
-file_size_mb=1000
-test_filename=speed_test_file
+for goal in 1 2 xor2 xor3; do
+	test_filename=speed_test_file_${goal}
+	touch "$test_filename"
+	mfssetgoal $goal "$test_filename"
 
-# write file and measure time
-time_file=$TEMP_DIR/$(unique_file)
-/usr/bin/time -o $time_file -f %e dd \
-	if=/dev/zero \
-	of=${test_filename} \
-	bs=128K \
-	count=$((file_size_mb * 8)) \
-	conv=fsync
+	# write file and measure time
+	drop_caches
+	time_file=$TEMP_DIR/$(unique_file)
+		/usr/bin/time -o "$time_file" -f %e dd \
+		if=/dev/zero \
+		of="$test_filename" \
+		bs=128K \
+		count=$((file_size_mb * 8)) \
+		conv=fsync
+	write_time=$(cat "$time_file")
+	write_speed=$(echo "scale=3;${file_size_mb}/${write_time}" | bc)
 
-write_time=$(<$time_file)
-write_speed=$(echo "scale=3;${file_size_mb}/${write_time}" | bc)
+	# read file and measure time
+	drop_caches
+	/usr/bin/time -o "$time_file" -f %e dd \
+		if="$test_filename" \
+		of=/dev/null \
+		bs=64K
+	read_time=$(cat "$time_file")
+	read_speed=$(echo "scale=3;${file_size_mb}/${read_time}" | bc)
 
-drop_caches
-
-# read file measuring time
-/usr/bin/time -o $time_file -f %e dd \
-	if=${test_filename} \
-	of=/dev/null bs=64K
-
-read_time=$(<$time_file)
-read_speed=$(echo "scale=3;${file_size_mb}/${read_time}" | bc)
-
-# write test results to csv file
-echo -e "Write speed,Read speed\n${write_speed}, ${read_speed}" | tee \
-	$TEST_OUTPUT_DIR/disk_speed_results.csv
+	# write test results to a csv file
+	echo -e "Write speed (goal $goal),Read speed (goal $goal)\n${write_speed}, ${read_speed}" \
+			| tee "${TEST_OUTPUT_DIR}/disk_speed_results_goal_${goal}.csv"
+done
--- tests/test_suites/Benchmarks/test_ramdisk_speed.sh
+++ tests/test_suites/Benchmarks/test_ramdisk_speed.sh
@@ -1,37 +1,43 @@
-timeout_set 1 minute
+timeout_set 3 minutes
 
-CHUNKSERVERS=1 \
-	USE_RAMDISK="yes" \
+CHUNKSERVERS=3 \
+	USE_RAMDISK=YES \
 	setup_local_empty_lizardfs info
 
+file_size_mb=800
 cd "${info[mount0]}"
 
-# file size in 1MB blocks
-file_size_mb=1000
-test_filename=speed_test_file
+for goal in 1 2 xor2 xor3; do
+	test_filename=speed_test_file_${goal}
+	touch "$test_filename"
+	mfssetgoal $goal "$test_filename"
 
-# write file and measure time
-time_file=$TEMP_DIR/$(unique_file)
-/usr/bin/time -o $time_file -f %e dd \
-	if=/dev/zero \
-	of=${test_filename} \
-	bs=1M \
-	count=${file_size_mb} \
-	conv=fsync
+	# write file and measure time
+	drop_caches
+	time_file=$TEMP_DIR/$(unique_file)
+		/usr/bin/time -o "$time_file" -f %e dd \
+		if=/dev/zero \
+		of="$test_filename" \
+		bs=128K \
+		count=$((file_size_mb * 8)) \
+		conv=fsync
+	write_time=$(cat "$time_file")
+	write_speed=$(echo "scale=3;${file_size_mb}/${write_time}" | bc)
 
-write_time=$(<$time_file)
-write_speed=$(echo "scale=3;${file_size_mb}/${write_time}" | bc)
+	# read file and measure time
+	drop_caches
+	/usr/bin/time -o "$time_file" -f %e dd \
+		if="$test_filename" \
+		of=/dev/null \
+		bs=64K
+	read_time=$(cat "$time_file")
+	read_speed=$(echo "scale=3;${file_size_mb}/${read_time}" | bc)
 
-drop_caches
+	# write test results to a csv file
+	echo -e "Write speed (goal $goal),Read speed (goal $goal)\n${write_speed}, ${read_speed}" \
+			| tee "${TEST_OUTPUT_DIR}/ramdisk_speed_results_goal_${goal}.csv"
 
-# read file measuring time
-/usr/bin/time -o $time_file -f %e dd \
-	if=${test_filename} \
-	of=/dev/null bs=64K
-
-read_time=$(<$time_file)
-read_speed=$(echo "scale=3;${file_size_mb}/${read_time}" | bc)
-
-# write test results to csv file
-echo -e "Write speed,Read speed\n${write_speed}, ${read_speed}" | tee \
-	$TEST_OUTPUT_DIR/ramdisk_speed_results.csv
+	# wait for chunkservers to close chunks and remove them immediately
+	sleep 10
+	find_all_chunks | xargs rm -f
+done
