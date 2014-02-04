67e7437e91b5c6b71d11a95d63cc9ced0c3db53b
Kamil Pachulski <kamil.pachulski@skytechnology.pl>
2014-02-04 13:06:20 +0100

    tests: Add test_xor_write_corrupted
    This test checks if writing to xor chunks with one missing part works
    correctly.
    
    Change-Id: Id5278fb72e9e425cfe492c5f6fddc3d5cf184440

---
+++ tests/test_suites/SanityChecks/test_xor_write_corrupted.sh
@@ -0,0 +1,30 @@
+CHUNKSERVERS=4 \
+	MOUNT_EXTRA_CONFIG="mfscachemode=NEVER" \
+	USE_RAMDISK=YES \
+	setup_local_empty_lizardfs info
+
+pseudorandom_init
+
+cd "${info[mount0]}"
+mkdir dir
+mfssetgoal xor3 dir
+cd dir
+
+for i in {0..19} ; do
+	filesize=$( pseudorandom 8 $((6 * LIZARDFS_BLOCK_SIZE)) )
+	head -c $filesize </dev/urandom >file${i}_$filesize
+done
+
+mfschunkserver -c "${info[chunkserver0_config]}" stop
+
+for file in * ; do
+	MESSAGE="Overwriting $file" expect_success file-overwrite $file
+	MESSAGE="Validating overwritten file" expect_success file-validate $file
+done
+
+mfschunkserver -c "${info[chunkserver0_config]}" start
+sleep 1
+
+for file in * ; do
+	MESSAGE="Validating $file after restart" expect_success file-validate $file
+done
