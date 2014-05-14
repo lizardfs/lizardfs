08c7eb28965a92aef87bf093c5579c0d396ff73c
Marcin Sulikowski <marcin.sulikowski@skytechnology.pl>
2014-05-14 11:00:23 +0200

    tests: Fix valgrind reporting some false positives
    Because of the design of gcc's std::string (its pointer does not point
    to the beginning of the allocated memory block, but somewhere inside it)
    memcheck reports false memory leaks when program exists with some
    strings still existing. New versions of memcheck (>= 3.9.0) have some
    heuristics which make it possible to detect that memory allocated for
    a reachable std::string is still reachable. To enable this heuristics
    one have to pass the --leak-check-heuristics flag. This commit modifies
    the testing framework to use this flag if available.
    
    Change-Id: Ib2e09273a87331e7861bdf7638298f0f6c78fb63

--- tests/tools/valgrind.sh
+++ tests/tools/valgrind.sh
@@ -1,19 +1,26 @@
 # Not enabled yet.
-valgrind_enabled=
+valgrind_enabled_=
 
 # Enables valgrind, can be called at the beginning of a test case.
-enable_valgrind () {
-	if ! is_program_installed valgrind; then
-		test_fail "valgrind not installed"
-	fi
-	if [[ -z $valgrind_enabled ]]; then
-		valgrind_enabled=1
+enable_valgrind() {
+	assert_program_installed valgrind
+	if [[ -z $valgrind_enabled_ ]]; then
+		valgrind_enabled_=1
+
+		# Build a valgrind invocation which will properly run memcheck.
+		local valgrind_command="valgrind -q --tool=memcheck --leak-check=full"
+
+		# New ( >= 3.9) versions of valgrind support some nice heuristics which remove
+		# a couple of false positives (eg. leak reports when there is a reachable std::string).
+		# Use the heuristics if available.
+		if valgrind --leak-check-heuristics=all true &>/dev/null; then
+			valgrind_command+=" --leak-check-heuristics=all"
+		fi
 
 		# Valgrind error messages will be written here.
-		valgrind_log="${ERROR_DIR}/valgrind_%p.log"
-		valgrind_command="valgrind -q --leak-check=full --log-file=${valgrind_log}"
+		valgrind_command+=" --log-file=${ERROR_DIR}/valgrind_%p.log"
 
-		echo --- valgrind enabled in this test case ---
+		echo " --- valgrind enabled in this test case ($(valgrind --version)) --- "
 		command_prefix="${valgrind_command} ${command_prefix}"
 		timeout_set_multiplier 10 # some tests need so big one
 	fi
