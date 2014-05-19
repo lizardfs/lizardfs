2067f59d4a7b980befe7967092fac0ebf5a83951
Kamil Pachulski <kamil.pachulski@skytechnology.pl>
2014-05-19 10:24:41 +0200

    common: Add goal definitions
    Until now, validation checking was performed by a comparison with numbers
    1 and 9. This commit defines values of minimal and maximal goal and validating
    function for goal checking simplification.
    
    Change-Id: I9bb1c764246ee92798440e9af90eba438cf51c01

---
+++ src/common/goal.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "config.h"
+
+#include <cstdint>
+
+constexpr uint8_t kMinGoal = 1;
+constexpr uint8_t kMaxGoal = 9;
+
+inline bool isGoalValid(uint8_t goal) {
+	return goal >= kMinGoal && goal <= kMaxGoal;
+}
---
+++ src/common/goal_unittest.cc
@@ -0,0 +1,15 @@
+#include "config.h"
+#include "common/goal.h"
+
+#include <gtest/gtest.h>
+
+TEST(GoalTests, IsGoalValid) {
+	for (int goal = 0; goal <= std::numeric_limits<uint8_t>::max(); ++goal) {
+		SCOPED_TRACE("Testing goal " + std::to_string(goal));
+		if (goal >= 1 && goal <= 9) {
+			EXPECT_TRUE(isGoalValid(goal));
+		} else {
+			EXPECT_FALSE(isGoalValid(goal));
+		}
+	}
+}
