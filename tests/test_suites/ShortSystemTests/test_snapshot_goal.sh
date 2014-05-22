cf99ec9bbf1e2586c227d0502c938fe9e7c995b2
Marcin Sulikowski <marcin.sulikowski@skytechnology.pl>
2014-05-22 20:36:59 +0200

    tests: Extend tests of snapshots to test xor goals
    Change-Id: I0d1b720569f0d1c9e717cad76cdb68d764feabb0

--- tests/test_suites/ShortSystemTests/test_snapshot_goal.sh
+++ tests/test_suites/ShortSystemTests/test_snapshot_goal.sh
@@ -1,9 +1,21 @@
 timeout_set '5 minutes'
 
-CHUNKSERVERS=6 \
+# Returns number of standard chunks and number of different parts for each xor level.
+# If the same chunk has two copies of the same parts, these will be counted as one, eg:
+# '3 standard' -- just 3 standard chunks
+# '2 standard 3 xor2' -- 2 standard chunks and 3 xor2 chunks
+# '6 xor5' -- 6 xor5 chunks (each is different)
+chunks_state() {
+	{
+		find_all_chunks | grep -o chunk_.*mfs | grep -o chunk_00000 | sed -e 's/.*/standard/'
+		find_all_chunks | grep -o chunk_.*mfs | sort -u | grep -o '_of_[2-9]' | sed -e 's/_of_/xor/'
+	} | sort | uniq -c | tr '\n' ' ' | trim_hard
+}
+
+CHUNKSERVERS=9 \
 	USE_RAMDISK=YES \
 	MOUNT_EXTRA_CONFIG="mfscachemode=NEVER" \
-	MASTER_EXTRA_CONFIG="CHUNKS_LOOP_TIME = 3|REPLICATIONS_DELAY_INIT = 0" \
+	MASTER_EXTRA_CONFIG="CHUNKS_LOOP_TIME = 5|REPLICATIONS_DELAY_INIT = 0|ACCEPTABLE_DIFFERENCE = 2" \
 	setup_local_empty_lizardfs info
 
 # Create a file consisting of 2 chunks of goal 4 (8 copies in total)
@@ -12,35 +24,33 @@ touch file
 mfssetgoal 4 file
 FILE_SIZE=$((1000 + LIZARDFS_CHUNK_SIZE)) file-generate file
 assert_success file-validate file
-assert_equals 8 $(find_all_chunks | wc -l)
+assert_equals "8 standard" "$(chunks_state)"
 
-# Create its snapshots in goal 1 and 2; there should be still 8 chunk files in total
+# Create its snapshots in goal 1 and xor2; there should be still 8 chunk files in total
 mfsmakesnapshot file file_snapshot1
 mfsmakesnapshot file file_snapshot2
 mfssetgoal 1 file_snapshot1
-mfssetgoal 2 file_snapshot2
-assert_equals 8 $(find_all_chunks | wc -l)
+mfssetgoal xor2 file_snapshot2
+assert_equals "8 standard" "$(chunks_state)"
 
-# Remove file leaving only snapshots; number of copies should decrease to 4 (goal 2)
+# Remove file leaving only snapshots; there should be 3 xor2 parts created for 2 chunks (6 total)
 mfssettrashtime 0 file*
 rm file
-echo "Waiting for chunks to be deleted..."
-assert_success wait_for '[[ $(mfsfileinfo file_snapshot2 | grep -c copy) == 4 ]]' '3 minutes'
-echo "Checking if chunks are no longer being deleted..."
-assert_failure wait_for '[[ $(mfsfileinfo file_snapshot2 | grep -c copy) != 4 ]]' '30 seconds'
-assert_equals 4 $(mfsfileinfo file_snapshot1 | grep -c copy)
+echo "Waiting for chunks to be converted..."
+assert_success wait_for '[[ "$(chunks_state)" == "6 xor2" ]]' '3 minutes'
+echo "Checking if chunks are no longer being converted/deleted..."
+assert_failure wait_for '[[ "$(chunks_state)" != "6 xor2" ]]' '30 seconds'
 
 # Remove file with goal 1, expect no deletions
 rm file_snapshot1
-echo "Checking if chunks are not being deleted..."
-assert_failure wait_for '[[ $(mfsfileinfo file_snapshot2 | grep -c copy) != 4 ]]' '30 seconds'
+echo "Checking if chunks are no longer being converted/deleted..."
+assert_failure wait_for '[[ "$(chunks_state)" != "6 xor2" ]]' '30 seconds'
 
-# Make a new snapshot of goal 3, expect that number of copies increases to 6
+# Make a new snapshot of goal 3, expect 6 standard chunks (2 chunks x 3 copies)
 mfsmakesnapshot file_snapshot2 file_snapshot3
 mfssetgoal 3 file_snapshot3
-echo "Waiting for chunks to be replicated..."
-assert_success wait_for '[[ $(mfsfileinfo file_snapshot2 | grep -c copy) == 6 ]]' '30 seconds'
-assert_equals 6 $(mfsfileinfo file_snapshot3 | grep -c copy)
+echo "Waiting for chunks to be converted..."
+assert_success wait_for '[[ "$(chunks_state)" == "6 standard" ]]' '1 minute'
 
 # Verify if file's data isn't damaged
 assert_success file-validate file*
