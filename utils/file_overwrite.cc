99cb213d6e06b01042d55d1a5d6ef75a8367466e
Kamil Pachulski <kamil.pachulski@skytechnology.pl>
2014-05-23 18:22:31 +0200

    utils: Let file-(generate|overwrite) write to many files
    Change-Id: I5d14dc4920e44343b2934e673d94ad4ed7991ca9

--- utils/file_overwrite.cc
+++ utils/file_overwrite.cc
@@ -3,14 +3,16 @@
 #include "utils/data_generator.h"
 
 int main(int argc, char** argv) {
-	if (argc != 2) {
+	if (argc < 2) {
 		std::cerr << "Usage:" << std::endl
-				<< "    " << argv[0] << " <file>" << std::endl
+				<< "    " << argv[0] << " <file>..." << std::endl
 				<< "Command uses the following environment variables: " << std::endl
 				<< "* BLOCK_SIZE" << std::endl;
 		return 1;
 	}
 
-	DataGenerator::overwriteFile(argv[1]);
+	for (int i = 1; i < argc; ++i) {
+		DataGenerator::overwriteFile(argv[i]);
+	}
 	return 0;
 }
