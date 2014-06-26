24c7544d79678c430f371aa75ec3294cb0c036f6
Marcin Konarski <amok@skytechnology.pl>
2014-06-26 12:53:32 +0200

    Revert "common: Provide (...) toString for Cygwin"
    Revert "common: Provide an implementetion of toString for Cygwin"
    This reverts commit 9f9e9157f5475d4e9d251138a090b9a408e9b5fa.
    
    Change-Id: I238f955389e2e05364ae1792536d972e3d095615

--- src/common/to_string.h
+++ src/common/to_string.h
@@ -1,24 +1,9 @@
 #pragma once
 #include "config.h"
 
-#ifdef __CYGWIN__
-
-#include <sstream>
-
-template <class C>
-std::string toString(C value) {
-	std::stringstream ss;
-	ss << value;
-	return ss.str();
-}
-
-#else // !__CYGWIN__
-
 #include <string>
 
 template <class C>
 std::string toString(C value) {
 	return std::to_string(value);
 }
-
-#endif // __CYGWIN__
