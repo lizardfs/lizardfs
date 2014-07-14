0424a8715c11ea393268291b11e30c6cb908166d
Alek Lewandowski <alek.lewandowski@skytechnology.pl>
2014-07-14 15:10:29 +0200

    tests: Test multiple master promotions
    Add a test in which N metaservers are running and the master personality
    is repeatedly moved from one metaserver to another.
    
    Change-Id:Ifbc9277a327cbc8cd361a0fa2e6bfb1d0b037a4e

---
+++ tests/test_suites/ShortSystemTests/test_multiple_master_promotions.sh
@@ -0,0 +1,46 @@
+timeout_set '2 minutes'
+
+metaservers_nr=5
+MASTERSERVERS=$metaservers_nr \
+	USE_RAMDISK="YES" \
+	MFSEXPORTS_EXTRA_OPTIONS="allcanchangequota" \
+	MOUNT_EXTRA_CONFIG="mfsacl,mfscachemode=NEVER"
+	setup_local_empty_lizardfs info
+
+wait_for_shadow_to_run_and_sync() {
+	shadow_id=$1
+	assert_eventually 'test -e "${info[master${shadow_id}_data_path]}/metadata.mfs.lock"'
+	assert_eventually 'test -e "${info[master${shadow_id}_data_path]}/changelog.mfs.3"'
+}
+
+# Start shadow masters
+for ((shadow_id=1 ; shadow_id<metaservers_nr; ++shadow_id)); do
+	lizardfs_master_n $shadow_id start
+	wait_for_shadow_to_run_and_sync $shadow_id
+done
+
+for ((loop_nr=0 ; loop_nr<$((2 * metaservers_nr)); ++loop_nr)); do
+	touch "${info[mount0]}"/"master=$loop_nr"
+	metadata=$(metadata_print "${info[mount0]}")
+
+	prev_master_id=$((loop_nr % metaservers_nr))
+	new_master_id=$(((loop_nr + 1) % metaservers_nr))
+
+	# Kill the previous master
+	lizardfs_master_daemon kill
+	lizardfs_make_conf_for_shadow $prev_master_id
+
+	# Promote a next master
+	lizardfs_make_conf_for_master $new_master_id
+	lizardfs_master_daemon reload
+
+	# Demote previous master to shadow
+	lizardfs_make_conf_for_shadow $prev_master_id
+	lizardfs_master_n $prev_master_id start
+	wait_for_shadow_to_run_and_sync $prev_master_id
+
+	lizardfs_wait_for_all_ready_chunkservers
+
+	MESSAGE="Data missmatch after switching masters $prev_master_id -> $new_master_id" \
+			assert_no_diff "$metadata" "$(metadata_print "${info[mount0]}")"
+done
