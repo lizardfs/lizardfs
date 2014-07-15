ff7eae9d0ba208dc426cafed6fbc03de88eb3b9c
Alek Lewandowski <alek.lewandowski@skytechnology.pl>
2014-07-15 13:03:30 +0200

    tests: Make test_flushing_changes_to_shadow test something
    Change-Id: Ie525af0222833e643b2e575a3aaa9ffae078278c

--- tests/test_suites/ShortSystemTests/test_flushing_changes_to_shadow.sh
+++ tests/test_suites/ShortSystemTests/test_flushing_changes_to_shadow.sh
@@ -1,12 +1,16 @@
-assert_program_installed pv
 assert_program_installed socat
 
 # start_proxy <listen> <forward>
 # Starts a proxy which listens on <listen> port, forwards all the traffic to <forward> port
-# and limits rate of replies to 75 KiB/s
+# and pauses the traffic when file $TEMP_DIR/gogo does not exist
 start_proxy() {
 	local ip=$(get_ip_addr)
-	local limiter_script="socat stdio tcp\:$ip\:$2\,rcvbuf=1024\,sndbuf=1024 | pv -qL 75k"
+	local limiter_script="socat stdio tcp\:$ip\:$2\,rcvbuf=1024\,sndbuf=1024 | `
+			`while true; do `
+				`if test -e \"$TEMP_DIR/gogo\"; then `
+					`dd bs=1024 count=1 2>/dev/null; else sleep 0.1; `
+				`fi; `
+			`done"
 	( socat "tcp-listen:$1,reuseaddr,rcvbuf=1024,sndbuf=1024" "system:${limiter_script}" & )
 }
 
@@ -18,9 +22,10 @@ CHUNKSERVERS=1 \
 m=${info[master0_data_path]} # master's working directory
 s=${info[master1_data_path]} # shadow's working directory
 
-# Create a proxy which will limit transfter rate of changelogs from master to shadow
-# and modify shadow's config to use this proxy.
+# Create a proxy which will pause the traffic from master to shadow and modify shadow's
+# config to use this proxy.
 get_next_port_number proxy_port
+touch "$TEMP_DIR/gogo";
 start_proxy "$proxy_port" "${info[matoml]}"
 sed -i -e "s/^MASTER_PORT.*/MASTER_PORT = $proxy_port/" "${info[master1_cfg]}"
 
@@ -28,10 +33,17 @@ sed -i -e "s/^MASTER_PORT.*/MASTER_PORT = $proxy_port/" "${info[master1_cfg]}"
 assert_success lizardfs_master_n 1 start
 assert_eventually 'test -e "$s/metadata.mfs.1"'
 
-# Generate about 1 MiB of entries and terminate the master server.
-# Expect that it will wait until all the changes are sent to the shadow master.
+# Pause traffic from master to shadow
+rm "$TEMP_DIR/gogo"
+
 mkdir "${info[mount0]}"/directory_with_name_long_enough_to_generate_a_big_changelog_entry{1..10000}
+begin_ts=$(timestamp)
+# Wait 3 seconds and resume the traffic between master and shadow
+( sleep 3; touch "$TEMP_DIR/gogo"; ) &
 assert_success lizardfs_master_daemon stop
 
-# Socket buffers need to be flushed, files have to be flushed, etc. Needs a couple of seconds more.
 assert_eventually 'cmp <(get_changes "$m" |tail) <(get_changes "$s" |tail) &>/dev/null'
+end_ts=$(timestamp)
+duration=$((end_ts - begin_ts))
+# The traffic was paused for 3 seconds, let's check if we didn't succeed too soon
+assert_less_or_equal 3 $duration
