8eae2555ea34bed88f8803700e52f37fca97bbe1
Marcin Sulikowski <marcin.sulikowski@skytechnology.pl>
2014-08-05 17:46:05 +0200

    utils: Add 'count' and 'usleep' arguments for mfsping
    These two options make it possible to configure how many and how often
    the messages should be sent which makes the tool much more usable.
    
    Change-Id: I05c6211c57857908777dec65878a95e2cfeb9d0a

--- utils/mfs_ping.cc
+++ utils/mfs_ping.cc
@@ -1,4 +1,5 @@
 #include <sys/time.h>
+#include <unistd.h>
 #include <iostream>
 #include <string>
 
@@ -10,13 +11,15 @@
 
 int main(int argc, char **argv) {
 	strerr_init();
-	if (argc != 4) {
-		std::cerr << "Usage: " << argv[0] << " host port bytes" << std::endl;
+	if (argc != 6) {
+		std::cerr << "Usage: " << argv[0] << " host port bytes count usleep" << std::endl;
 		return 1;
 	}
 	uint32_t ip;
 	uint16_t port;
 	uint32_t size = atoi(argv[3]);
+	uint32_t count = atoi(argv[4]);
+	uint32_t sleep_us = atoi(argv[5]);
 	eassert(tcpresolve(argv[1], argv[2], &ip, &port, 0) == 0);
 	sassert(std::to_string(size) == argv[3]);
 
@@ -30,16 +33,20 @@ int main(int argc, char **argv) {
 
 	std::vector<uint8_t> replyBuffer(size + 8);
 	uint64_t microseconds = 0;
-	int n = 10000;
-	for (int i = 0; i < n; ++i) {
+	for (uint32_t i = 1; i <= count; ++i) {
 		struct timeval start, stop;
 		gettimeofday(&start, 0);
 		eassert(tcptowrite(fd, message.data(), message.size(), 10000) == (ssize_t)message.size());
 		eassert(tcptoread(fd, replyBuffer.data(), replyBuffer.size(), 10000) == (ssize_t)replyBuffer.size());
 		gettimeofday(&stop, 0);
-		microseconds += (stop.tv_usec + stop.tv_sec * 1000000);
-		microseconds -= (start.tv_usec + start.tv_sec * 1000000);
+		uint64_t elapsed = (stop.tv_usec + stop.tv_sec * 1000000);
+		elapsed -= (start.tv_usec + start.tv_sec * 1000000);
+		std::cout << elapsed << " us" << std::endl;
+		microseconds += elapsed;
+		if (sleep_us > 0) {
+			usleep(sleep_us);
+		}
 	}
-	std::cout << "Avg: " << microseconds / n << " us" <<std::endl;
+	std::cout << "Avg: " << microseconds / count << " us" << std::endl;
 	return 0;
 }
