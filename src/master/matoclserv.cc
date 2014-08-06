2bcf75371eeaa4f34ed14f9af02c0825ebf27b85
Kamil Pachulski <kamil.pachulski@skytechnology.pl>
2014-08-06 17:40:29 +0200

    tests: Add test of ACLs' cache
    Test if cache timeout and cache max capacity work properly.
    
    Change-Id: I4dfeb008749cacff09c9cb86efb903241283d793

--- src/master/matoclserv.cc
+++ src/master/matoclserv.cc
@@ -3341,6 +3341,7 @@ void matoclserv_fuse_getacl(matoclserventry *eptr, const uint8_t *data, uint32_t
 	uint32_t messageId, inode, uid, gid;
 	AclType type;
 	cltoma::fuseGetAcl::deserialize(data, length, messageId, inode, uid, gid, type);
+	DEBUG_LOG("master.matocl.getacl") << inode;
 
 	MessageBuffer reply;
 	AccessControlList acl;
