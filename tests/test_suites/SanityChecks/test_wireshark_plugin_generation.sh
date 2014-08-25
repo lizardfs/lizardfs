a0914e9b6b71f079c370a8703ccfc773093b9c0f
Marcin Konarski <amok@skytechnology.pl>
2014-08-25 20:26:54 +0200

    tests: Skip wireshark plugin creation if python3 is absent
    Change-Id: I7c087375063e5918d9456297c91de7e9c2d3206d

--- tests/test_suites/SanityChecks/test_wireshark_plugin_generation.sh
+++ tests/test_suites/SanityChecks/test_wireshark_plugin_generation.sh
@@ -2,5 +2,9 @@ cd "$TEMP_DIR"
 cp -a "$SOURCE_DIR/utils/wireshark/plugins/lizardfs" .
 rm -f lizardfs/*.c
 
-assert_success lizardfs/generate.sh "$SOURCE_DIR/src/common/MFSCommunication.h"
-assert_success test -s lizardfs/packet-lizardfs.c
+if is_program_installed python3 ; then
+	assert_success lizardfs/generate.sh "$SOURCE_DIR/src/common/MFSCommunication.h"
+	assert_success test -s lizardfs/packet-lizardfs.c
+else
+	echo "python3 is not installed on your system hence wireshark plugin won't be build."
+fi
