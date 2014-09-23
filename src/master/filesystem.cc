e96fd226ddda90edba4dc2e90f6e7b3718c36d25
Kamil Pachulski <kamil.pachulski@skytechnology.pl>
2014-09-23 15:50:43 +0200

    master: Add SAVE_METADATA_AT_EXIT config option
    This commit adds a config option to master which allows to change if metadata
    is dumped on "stop" or "restart" action. This feature is needed for tests of
    changing master server because sometimes master server was being killed after
    performing the clients requests but before sending them a confirmation.
    
    Change-Id: I77613c9912c3f346f161923df9a5485c71f1d5c6

--- src/master/filesystem.cc
+++ src/master/filesystem.cc
@@ -471,6 +471,8 @@ void fs_stats(uint32_t stats[16]) {
 	stats_write=0;
 }
 
+static bool gSaveMetadataAtExit = true;
+
 #endif // ifndef METARESTORE
 
 // Number of changelog file versions
@@ -8064,7 +8066,8 @@ void fs_term(void) {
 		metadataDumper.waitUntilFinished();
 	}
 	for (;;) {
-		if (gMetadata == nullptr || fs_storeall(MetadataDumper::kForegroundDump)) {
+		if (gMetadata == nullptr || !gSaveMetadataAtExit ||
+				fs_storeall(MetadataDumper::kForegroundDump)) {
 			break;
 		}
 		syslog(LOG_ERR,"can't store metadata - try to make more space on your hdd or change privieleges - retrying after 10 seconds");
@@ -8072,7 +8075,9 @@ void fs_term(void) {
 	}
 	chunk_unload();
 	// delete gMetadata; // We may do this, but it would slow down restarts of the master server
-	fs_unlock();
+	if (gSaveMetadataAtExit) {
+		fs_unlock();
+	}
 }
 
 #else
@@ -8222,6 +8227,7 @@ static void fs_read_config_file() {
 	gAutoRecovery = cfg_getint32("AUTO_RECOVERY", 0) == 1;
 	gDisableChecksumVerification = cfg_getint32("DISABLE_METADATA_CHECKSUM_VERIFICATION", 0) != 0;
 	gMagicAutoFileRepair = cfg_getint32("MAGIC_AUTO_FILE_REPAIR", 0) == 1;
+	gSaveMetadataAtExit = cfg_getint32("SAVE_METADATA_AT_EXIT", 1) != 0;
 	gStoredPreviousBackMetaCopies = cfg_get_maxvalue(
 			"BACK_META_KEEP_PREVIOUS",
 			kDefaultStoredPreviousBackMetaCopies,
