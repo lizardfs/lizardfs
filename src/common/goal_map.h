12c679e731c107d005e5cf8e4b69de9408c927b1
Alek Lewandowski <alek.lewandowski@skytechnology.pl>
2014-10-15 13:13:47 +0200

    common: Change GoalMap serialization
    Make GoalMap use std::map instead of fixed size array when serializing.
    This change will make it possible to pass GoalMaps through the network
    between two modules in two diffrent version, being aware of diffrent
    number of possible goals.
    
    Change-Id: I7a23e809f7fdfc60cd6b390cfe2311470abc3e4f

--- src/common/goal_map.h
+++ src/common/goal_map.h
@@ -3,6 +3,7 @@
 #include "common/platform.h"
 
 #include <cstdint>
+#include <map>
 
 #include "common/exception.h"
 #include "common/goal.h"
@@ -29,9 +30,39 @@ public:
 		return const_cast<GoalMap&>(*this)[goal];
 	}
 
-	LIZARDFS_DEFINE_SERIALIZE_METHODS(zero_, goals_);
+	uint32_t serializedSize() const {
+		auto map = goalsAsMap();
+		return ::serializedSize(zero_, map);
+	}
+	void serialize(uint8_t** destination) const {
+		auto map = goalsAsMap();
+		::serialize(destination, zero_, map);
+	}
+	void deserialize(const uint8_t** source, uint32_t& bytesLeftInBuffer) {
+		T defaultValue = T();
+		sassert(zero_ == defaultValue);
+
+		std::map<uint8_t, T> map;
+		::deserialize(source, bytesLeftInBuffer, zero_, map);
+
+		for (unsigned i = goal::kMinGoal; i <= goal::kMaxGoal; ++i) {
+			sassert(this->operator[](i) == defaultValue);
+			this->operator[](i) = map[i];
+		}
+	}
 
 private:
+	std::map<uint8_t, T> goalsAsMap() const {
+		std::map<uint8_t, T> trueMap;
+		T defaultValue = T();
+		for (unsigned i = goal::kMinGoal; i <= goal::kMaxGoal; ++i) {
+			if (this->operator[](i) != defaultValue) { // Packet size optimization!
+				trueMap[i] = this->operator[](i);
+			}
+		}
+		return trueMap;
+	}
+
 	T zero_;
 	T goals_[goal::kNumberOfGoals];
 };
