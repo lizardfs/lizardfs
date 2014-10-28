6c1078b0456d1d339757d101ee49c9339c13620f
Marcin Sulikowski <marcin.sulikowski@skytechnology.pl>
2014-10-28 12:10:03 +0100

    tests: Add unit tests which verify distribution of new chunks
    This commit adds a unit test which verifies if new chunks are placed on
    servers with respect to their weights.
    
    Change-Id: I0886b1410e0aa474c04add6dc24a79de85ef68a8

--- src/master/get_servers_for_new_chunk.h
+++ src/master/get_servers_for_new_chunk.h
@@ -9,6 +9,9 @@
 
 struct matocsserventry;
 
+/// A struct used to remember how many chunks were created on a given server.
+/// We remember the server's data (pointer, label, weight) to be able to verify if this
+/// information didn't change.
 struct ChunkserverChunkCounter {
 	ChunkserverChunkCounter(matocsserventry* server, MediaLabel label, int64_t weight)
 			: server(server),
@@ -20,17 +23,26 @@ struct ChunkserverChunkCounter {
 	matocsserventry* server;
 	MediaLabel label;
 	int64_t weight;
+
+	/// Number of chunks created on this sever.
+	/// This information would be reset if anything did change (eg. list of servers,
+	/// their labels or weights).
 	int64_t chunksCreatedSoFar;
 };
 
+/// History of chunk creation for some goal
 struct ChunkCreationHistory {
 	ChunkCreationHistory() {}
 	ChunkCreationHistory(Goal goal) : goal(std::move(goal)) {}
 
+	/// The goal.
 	Goal goal;
+
+	/// Information about chunks created on each server for the \p goal.
 	std::vector<ChunkserverChunkCounter> servers;
 };
 
+/// An algorithm which chooses servers for a new chunk.
 class GetServersForNewChunk {
 public:
 	struct WeightedServer {
