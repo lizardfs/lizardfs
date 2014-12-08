lizardfs/lizardfs

commit e701e69bac1a4bed448687ea61d467ae767e9c39

master, chunkserver, metarestore: Improve startup messages
This commit improves most of the messages printed on startup to make it
more clear what the user should do when something goes wrong, e.g.:

  init: hdd space manager failed !!!

is changed to:

  [FAIL] can't open hdd config file /etc/mfs/mfshdd.cfg: ENOENT (No such file or directory) - new file can be created using /etc/mfs/mfshdd.cfg.dist

Change-Id: Iaeffbbabf1a7f322f29e416e7101ab909b3ba8ff

src/main/main.cc
@@ -457,11 +457,11 @@ int initialize(void) {
 		now = time(NULL);
 		try {
 			if (RunTab[i].fn() < 0) {
-				lzfs_pretty_syslog(LOG_ERR, "init: %s failed !!!", RunTab[i].name);
+				lzfs_pretty_syslog(LOG_ERR, "init: %s failed", RunTab[i].name);
 				ok=0;
 			}
 		} catch (const std::exception& e) {
-			lzfs_pretty_syslog(LOG_ERR, "init: %s failed: %s !!!", RunTab[i].name, e.what());
+			lzfs_pretty_syslog(LOG_ERR, "%s", e.what());
 			ok = 0;
 		}
 	}
@@ -474,9 +474,14 @@ int initialize_late(void) {
 	ok = 1;
 	for (i=0 ; (long int)(LateRunTab[i].fn)!=0 && ok ; i++) {
 		now = time(NULL);
-		if (LateRunTab[i].fn()<0) {
-			lzfs_pretty_syslog(LOG_ERR,"init: %s failed !!!",RunTab[i].name);
-			ok=0;
+		try {
+			if (LateRunTab[i].fn()<0) {
+				lzfs_pretty_syslog(LOG_ERR,"init: %s failed",RunTab[i].name);
+				ok=0;
+			}
+		} catch (const std::exception& e) {
+			lzfs_pretty_syslog(LOG_ERR, "%s", e.what());
+			ok = 0;
 		}
 	}
 	now = time(NULL);
@@ -492,9 +497,11 @@ const std::string& set_syslog_ident() {
 	closelog();
 	if (gRunAsDaemon) {
 		openlog(logIdent.c_str(), LOG_PID | LOG_NDELAY, LOG_DAEMON);
+		lzfs_disable_printf();
 	} else {
 #if defined(LOG_PERROR)
 		openlog(logIdent.c_str(), LOG_PID | LOG_NDELAY | LOG_PERROR, LOG_USER);
+		lzfs_disable_printf();
 #else
 		openlog(logIdent.c_str(), LOG_PID | LOG_NDELAY, LOG_USER);
 #endif
@@ -661,7 +668,7 @@ void changeugid(void) {
 		} else if (wgroup[0]) {
 			getgrnam_r(wgroup,&grp,pwdgrpbuff,16384,&gr);
 			if (gr==NULL) {
-				lzfs_pretty_syslog(LOG_WARNING,"%s: no such group !!!",wgroup);
+				lzfs_pretty_syslog(LOG_WARNING,"%s: no such group",wgroup);
 				exit(LIZARDFS_EXIT_STATUS_ERROR);
 			} else {
 				wrk_gid = gr->gr_gid;
@@ -682,7 +689,7 @@ void changeugid(void) {
 		} else {
 			getpwnam_r(wuser,&pwd,pwdgrpbuff,16384,&pw);
 			if (pw==NULL) {
-				lzfs_pretty_syslog(LOG_ERR,"%s: no such user !!!",wuser);
+				lzfs_pretty_syslog(LOG_ERR,"%s: no such user",wuser);
 				exit(LIZARDFS_EXIT_STATUS_ERROR);
 			}
 			wrk_uid = pw->pw_uid;
@@ -789,7 +796,7 @@ void FileLock::createLockFile() {
 	bool notExisted((::access(name_.c_str(), F_OK) != 0) && (errno == ENOENT));
 	fd_.reset(open(name_.c_str(), O_WRONLY | O_CREAT, 0666));
 	if (!fd_.isOpened()) {
-		throw FilesystemException("can't create lockfile in working directory");
+		throw FilesystemException("can't create lockfile " + name_ + " in working directory ");
 	}
 	thisProcessCreatedLockFile_ = notExisted;
 }
@@ -798,7 +805,7 @@ FileLock::LockStatus FileLock::wdlock(RunMode runmode, uint32_t timeout) {
 	createLockFile();
 	pid_t ownerpid(mylock());
 	if (ownerpid<0) {
-		lzfs_pretty_errlog(LOG_ERR,"fcntl error");
+		lzfs_pretty_errlog(LOG_ERR,"fcntl error while creating lockfile %s in working directory", name_.c_str());
 		return LockStatus::kFail;
 	}
 	if (ownerpid>0) {
@@ -810,7 +817,7 @@ FileLock::LockStatus FileLock::wdlock(RunMode runmode, uint32_t timeout) {
 			return LockStatus::kFail;
 		}
 		if (runmode==RunMode::kStart) {
-			fprintf(stderr,"can't start: lockfile is already locked by another process\n");
+			lzfs_pretty_syslog(LOG_ERR,"can't start: lockfile %s in working directory is already locked by another process", name_.c_str());
 			return LockStatus::kFail;
 		}
 		if (runmode==RunMode::kReload) {
@@ -818,14 +825,13 @@ FileLock::LockStatus FileLock::wdlock(RunMode runmode, uint32_t timeout) {
 			 * FIXME: buissiness logic should not be in file locking function.
 			 */
 			if (kill(ownerpid,SIGHUP)<0) {
-				lzfs_pretty_errlog(LOG_WARNING,"can't send reload signal to lock owner");
+				lzfs_pretty_errlog(LOG_ERR,"can't send reload signal to lock owner of %s in working directory", name_.c_str());
 				return LockStatus::kFail;
 			}
-			fprintf(stderr,"reload signal has beed sent\n");
+			lzfs_pretty_syslog(LOG_INFO,"reload signal has been sent; lockfile %s created in working directory and locked", name_.c_str());
 			return LockStatus::kSuccess;
 		}
 		if (runmode==RunMode::kKill) {
-			fprintf(stderr,"sending SIGKILL to lock owner (pid:%ld)\n",(long int)ownerpid);
 			/*
 			 * FIXME: buissiness logic should not be in file locking function.
 			 */
@@ -834,21 +840,19 @@ FileLock::LockStatus FileLock::wdlock(RunMode runmode, uint32_t timeout) {
 #else
 			if (kill(ownerpid,SIGKILL)<0) {
 #endif
-				lzfs_pretty_errlog(LOG_WARNING,"can't kill lock owner");
+				lzfs_pretty_errlog(LOG_ERR,"can't kill (SIGKILL) lock owner (pid:%ld) of %s in working directory",(long int)ownerpid, name_.c_str());
 				return LockStatus::kFail;
 			}
 		} else {
 			sassert((runmode == RunMode::kStop) || (runmode == RunMode::kRestart));
-			fprintf(stderr,"sending SIGTERM to lock owner (pid:%ld)\n",(long int)ownerpid);
 			/*
 			 * FIXME: buissiness logic should not be in file locking function.
 			 */
 			if (kill(ownerpid,SIGTERM)<0) {
-				lzfs_pretty_errlog(LOG_WARNING,"can't kill lock owner");
+				lzfs_pretty_errlog(LOG_ERR,"can't kill (SIGTERM) lock owner (pid:%ld)  of %s in working directory",(long int)ownerpid, name_.c_str());
 				return LockStatus::kFail;
 			}
 		}
-		fprintf(stderr,"waiting for termination ... ");
 		fflush(stderr);
 		uint32_t l = 0;
 		pid_t newownerpid;
@@ -856,40 +860,35 @@ FileLock::LockStatus FileLock::wdlock(RunMode runmode, uint32_t timeout) {
 		do {
 			newownerpid = mylock();
 			if (newownerpid<0) {
-				lzfs_pretty_errlog(LOG_ERR,"fcntl error");
+				lzfs_pretty_errlog(LOG_ERR,"fcntl error while creating lockfile %s in working directory", name_.c_str());
 				return LockStatus::kFail;
 			}
 			if (newownerpid>0) {
 				l++;
 				uint32_t secondsElapsed = l / checksPerSecond;
 				if (secondsElapsed >= timeout) {
-					syslog(LOG_ERR,
-							"about %" PRIu32 " seconds passed and lock still exists - giving up",
-							secondsElapsed);
-					fprintf(stderr,"giving up\n");
+					lzfs_pretty_syslog(LOG_ERR, "about %" PRIu32 " seconds passed and lock %s still "
+								"exists in working directory - giving up",
+								secondsElapsed, name_.c_str());
 					return LockStatus::kFail;
 				}
 				if (l % (10 * checksPerSecond) == 0) {
-					syslog(LOG_WARNING, "about %" PRIu32 " seconds passed and lock still exists",
-							secondsElapsed);
-					fprintf(stderr,"%" PRIu32 "s ", secondsElapsed);
+					lzfs_pretty_syslog(LOG_WARNING, "about %" PRIu32 " seconds passed and lock %s "
+								"still exists in working directory - giving up",
+								secondsElapsed, name_.c_str());
 					fflush(stderr);
 				}
 				if (newownerpid!=ownerpid) {
-					fprintf(stderr,"\nnew lock owner detected\n");
+					lzfs_silent_syslog(LOG_INFO,"new lock owner detected");
 					if (runmode==RunMode::kKill) {
-						fprintf(stderr,"sending SIGKILL to lock owner (pid:%ld) ... ",(long int)newownerpid);
-						fflush(stderr);
 						if (kill(newownerpid,SIGKILL)<0) {
-							lzfs_pretty_errlog(LOG_WARNING,"can't kill lock owner");
+							lzfs_pretty_errlog(LOG_ERR,"can't kill (SIGKILL) lock owner (pid:%ld)",(long int)newownerpid);
 							return LockStatus::kFail;
 						}
 					} else {
 						sassert((runmode == RunMode::kStop) || (runmode == RunMode::kRestart));
-						fprintf(stderr,"sending SIGTERM to lock owner (pid:%ld) ... ",(long int)newownerpid);
-						fflush(stderr);
 						if (kill(newownerpid,SIGTERM)<0) {
-							lzfs_pretty_errlog(LOG_WARNING,"can't kill lock owner");
+							lzfs_pretty_errlog(LOG_ERR,"can't kill (SIGTERM) lock owner (pid:%ld)",(long int)newownerpid);
 							return LockStatus::kFail;
 						}
 					}
@@ -898,19 +897,18 @@ FileLock::LockStatus FileLock::wdlock(RunMode runmode, uint32_t timeout) {
 			}
 			usleep(1000000 / checksPerSecond);
 		} while (newownerpid!=0);
-		fprintf(stderr,"terminated\n");
 		return (runmode == RunMode::kRestart) ? LockStatus::kAgain : LockStatus::kSuccess;
 	}
 	if (runmode==RunMode::kStart || runmode==RunMode::kRestart) {
-		fprintf(stderr,"lockfile created and locked\n");
+		lzfs_pretty_syslog(LOG_INFO,"lockfile created and locked");
 	} else if (runmode==RunMode::kStop || runmode==RunMode::kKill) {
-		fprintf(stderr,"can't find process to terminate\n");
+		lzfs_pretty_syslog(LOG_WARNING,"can't find process to terminate");
 		return LockStatus::kSuccess;
 	} else if (runmode==RunMode::kReload) {
-		fprintf(stderr,"can't find process to send reload signal\n");
+		lzfs_pretty_syslog(LOG_ERR,"can't find process to send reload signal");
 		return LockStatus::kFail;
 	} else if (runmode==RunMode::kTest) {
-		fprintf(stderr,STR(APPNAME) " is not running\n");
+		fprintf(stderr, STR(APPNAME) " is not running\n");
 	} else if (runmode==RunMode::kIsAlive) {
 		return LockStatus::kSuccess;
 	}
@@ -927,18 +925,18 @@ void makedaemon() {
 	fflush(stdout);
 	fflush(stderr);
 	if (pipe(piped)<0) {
-		fprintf(stderr,"pipe error\n");
+		lzfs_pretty_syslog(LOG_ERR, "pipe error");
 		exit(LIZARDFS_EXIT_STATUS_ERROR);
 	}
 	f = fork();
 	if (f<0) {
-		syslog(LOG_ERR,"first fork error: %s",strerr(errno));
+		lzfs_pretty_errlog(LOG_ERR, "first fork error");
 		exit(LIZARDFS_EXIT_STATUS_ERROR);
 	}
 	if (f>0) {
 		wait(&f);       // just get child status - prevents child from being zombie during initialization stage
 		if (f) {
-			fprintf(stderr,"Child status: %d\n",f);
+			lzfs_pretty_syslog(LOG_ERR, "child status: %d",f);
 			exit(LIZARDFS_EXIT_STATUS_ERROR);
 		}
 		close(piped[1]);
@@ -954,7 +952,7 @@ void makedaemon() {
 				happy = fwrite(pipebuff,1,r,stderr);
 				(void)happy;
 			} else {
-				fprintf(stderr,"Error reading pipe: %s\n",strerr(errno));
+				lzfs_pretty_errlog(LOG_ERR,"error reading pipe");
 				exit(LIZARDFS_EXIT_STATUS_ERROR);
 			}
 		}
@@ -964,9 +962,9 @@ void makedaemon() {
 	setpgid(0,getpid());
 	f = fork();
 	if (f<0) {
-		syslog(LOG_ERR,"second fork error: %s",strerr(errno));
+		lzfs_pretty_errlog(LOG_ERR,"second fork error");
 		if (write(piped[1],"fork error\n",11)!=11) {
-			syslog(LOG_ERR,"pipe write error: %s",strerr(errno));
+			lzfs_pretty_errlog(LOG_ERR,"pipe write error");
 		}
 		close(piped[1]);
 		exit(LIZARDFS_EXIT_STATUS_ERROR);
@@ -1030,35 +1028,21 @@ void usage(const char *appname) {
 
 int main(int argc,char **argv) {
 	char *wrkdir;
-	char *cfgfile;
+	char *cfgfile=NULL;
 	char *appname;
 	int ch;
 	int logundefined;
 	int lockmemory;
 	int32_t nicelevel;
 	uint32_t locktimeout;
-	int fd;
-	uint8_t movewarning;
 	struct rlimit rls;
+	std::string name_new(ETC_PATH "/mfs/" STR(APPNAME) ".cfg");
+	std::string name_old(ETC_PATH "/" STR(APPNAME) ".cfg");
 
 	prepareEnvironment();
 	strerr_init();
 	mycrc32_init();
 
-	movewarning = 0;
-	cfgfile=strdup(ETC_PATH "/mfs/" STR(APPNAME) ".cfg");
-	passert(cfgfile);
-	if ((fd = open(cfgfile,O_RDONLY))<0 && errno==ENOENT) {
-		free(cfgfile);
-		cfgfile=strdup(ETC_PATH "/" STR(APPNAME) ".cfg");
-		passert(cfgfile);
-		if ((fd = open(cfgfile,O_RDONLY))>=0) {
-			movewarning = 1;
-		}
-	}
-	if (fd>=0) {
-		close(fd);
-	}
 	locktimeout = 1800;
 	RunMode runmode = RunMode::kRestart;
 	logundefined = 0;
@@ -1077,10 +1061,8 @@ int main(int argc,char **argv) {
 				locktimeout=strtoul(optarg,NULL,10);
 				break;
 			case 'c':
-				free(cfgfile);
 				cfgfile = strdup(optarg);
 				passert(cfgfile);
-				movewarning = 0;
 				break;
 			case 'u':
 				logundefined=1;
@@ -1119,8 +1101,18 @@ int main(int argc,char **argv) {
 		return LIZARDFS_EXIT_STATUS_ERROR;
 	}
 
-	if (movewarning) {
-		lzfs_pretty_syslog(LOG_WARNING,"default sysconf path has changed - please move " STR(APPNAME) ".cfg from " ETC_PATH "/ to " ETC_PATH "/mfs/");
+	if (cfgfile == NULL) { /*try loading default config files*/
+		cfgfile = strdup(name_new.c_str());
+		try {
+			if (!fs::exists(name_new) && fs::exists(name_old)) {
+				free(cfgfile);
+				cfgfile = strdup(name_old.c_str());
+				lzfs_pretty_syslog(LOG_WARNING, "default sysconf path has changed - please move " STR(APPNAME) ".cfg from " ETC_PATH "/ to " ETC_PATH "/mfs/");
+			}
+		} catch (const FilesystemException& ex) {
+			lzfs_pretty_syslog(LOG_WARNING, "can't access config file %s: %s",
+					name_new.c_str(), ex.what());
+		}
 	}
 
 	if (runmode==RunMode::kStart || runmode==RunMode::kRestart) {
@@ -1132,17 +1124,24 @@ int main(int argc,char **argv) {
 	}
 
 	if (cfg_load(cfgfile,logundefined)==0) {
-		fprintf(stderr,"can't load config file: %s - using defaults\n",cfgfile);
+		lzfs_pretty_syslog(LOG_WARNING, "configuration file %s not found - using defaults; "
+				"please create one to remove this warning "
+				"(you can copy %s.dist to get a base configuration)",
+				cfgfile, name_new.c_str());
+	} else if (runmode==RunMode::kStart || runmode==RunMode::kRestart) {
+		lzfs_pretty_syslog(LOG_INFO,"configuration file %s loaded", cfgfile);
 	}
 	free(cfgfile);
 	main_configure_debug_log();
-	const std::string& logappname = set_syslog_ident();
+// const std::string& logappname = set_syslog_ident();
 
 	if (runmode==RunMode::kStart || runmode==RunMode::kRestart) {
 		rls.rlim_cur = MFSMAXFILES;
 		rls.rlim_max = MFSMAXFILES;
 		if (setrlimit(RLIMIT_NOFILE,&rls)<0) {
-			syslog(LOG_NOTICE,"can't change open files limit to %u",MFSMAXFILES);
+			lzfs_pretty_syslog(LOG_WARNING,"can't change open files limit to %u, check restrictions with `ulimit -n'",MFSMAXFILES);
+		} else {
+			lzfs_pretty_syslog(LOG_INFO,"open files limit limit changed to to %u",MFSMAXFILES);
 		}
 
 		lockmemory = cfg_getnum("LOCK_MEMORY",0);
@@ -1160,9 +1159,7 @@ int main(int argc,char **argv) {
 	changeugid();
 
 	wrkdir = cfg_getstr("DATA_PATH",DATA_PATH);
-	if (runmode==RunMode::kStart || runmode==RunMode::kRestart) {
-		fprintf(stderr,"working directory: %s\n",wrkdir);
-	}
+
 
 	if (chdir(wrkdir)<0) {
 		lzfs_pretty_syslog(LOG_ERR,"can't set working directory to %s",wrkdir);
@@ -1172,6 +1169,10 @@ int main(int argc,char **argv) {
 		}
 		closelog();
 		return LIZARDFS_EXIT_STATUS_ERROR;
+	} else {
+		if (runmode==RunMode::kStart || runmode==RunMode::kRestart) {
+			lzfs_pretty_syslog(LOG_INFO,"changed working directory to: %s",wrkdir);
+		}
 	}
 	free(wrkdir);
 
@@ -1212,6 +1213,7 @@ int main(int argc,char **argv) {
 		}
 	}
 
+
 #ifdef MFS_USE_MEMLOCK
 	if (lockmemory) {
 		if (getrlimit(RLIMIT_MEMLOCK,&rls)<0) {
@@ -1235,21 +1237,21 @@ int main(int argc,char **argv) {
 					} else {
 						lzfs_pretty_syslog(LOG_NOTICE,"process memory was successfully locked in RAM");
 					}
-			}       }
+				}
+			}
 		}
 	}
 #else
 	if (lockmemory) {
-		lzfs_pretty_syslog(LOG_WARNING,"memory lock not supported !!!");
+		lzfs_pretty_syslog(LOG_WARNING,"trying to lock memory, but memory lock not supported");
 	}
 #endif
-	fprintf(stderr, "initializing %s modules ...\n", logappname.c_str());
-
 	if (initialize()) {
 		if (getrlimit(RLIMIT_NOFILE,&rls)==0) {
-			syslog(LOG_NOTICE,"open files limit: %lu",(unsigned long)(rls.rlim_cur));
+			lzfs_pretty_syslog(LOG_NOTICE,"open files limit: %lu",(unsigned long)(rls.rlim_cur));
 		}
-		fprintf(stderr, "%s daemon initialized properly\n", logappname.c_str());
+		lzfs_pretty_syslog(LOG_NOTICE, STR(APPNAME)" daemon initialized properly");
+		set_syslog_ident();
 		if (gRunAsDaemon) {
 			close_msg_channel();
 		}
@@ -1261,7 +1263,6 @@ int main(int argc,char **argv) {
 			ch=LIZARDFS_EXIT_STATUS_ERROR;
 		}
 	} else {
-		fprintf(stderr,"error occured during initialization - exiting\n");
 		if (gRunAsDaemon) {
 			fputc(0,stderr);
 			close_msg_channel();

src/metarestore/main.cc
@@ -236,12 +236,13 @@ int main(int argc,char **argv) {
 			}
 		}
 		if (bestmetadata.empty()) {
-			lzfs_pretty_syslog(LOG_ERR, "error: can't find backed up metadata file !!!");
+			lzfs_pretty_syslog(LOG_ERR, "can't find backed up metadata file");
 			return 1;
 		}
 		metadata = bestmetadata;
 		metaout =  datapath + "/" + kMetadataFilename;
-		fprintf(stderr, "file %s will be used to restore the most recent metadata\n", metadata.c_str());
+		lzfs_pretty_syslog(LOG_INFO, "file %s will be used to restore the most recent metadata",
+				metadata.c_str());
 	}
 	try {
 		if (fs_init(metadata.c_str(), ignoreflag, noLock) != 0) {

src/chunkserver/csserv.cc
@@ -33,8 +33,10 @@
 #include "chunkserver/bgjobs.h"
 #include "chunkserver/hddspacemgr.h"
 #include "common/cfg.h"
+#include "common/cwrap.h"
 #include "common/charts.h"
 #include "common/datapack.h"
+#include "common/exceptions.h"
 #include "common/main.h"
 #include "common/massert.h"
 #include "common/MFSCommunication.h"
@@ -1654,8 +1656,8 @@ int csserv_init(void) {
 
 	lsock = tcpsocket();
 	if (lsock<0) {
-		lzfs_pretty_errlog(LOG_ERR,"main server module: can't create socket");
-		return -1;
+		throw InitializeException("main server module: can't create socket :" +
+				errorString(errno));
 	}
 	tcpnonblock(lsock);
 	tcpnodelay(lsock);
@@ -1665,8 +1667,8 @@ int csserv_init(void) {
 	}
 	tcpresolve(ListenHost,ListenPort,&mylistenip,&mylistenport,1);
 	if (tcpnumlisten(lsock,mylistenip,mylistenport,100)<0) {
-		lzfs_pretty_errlog(LOG_ERR,"main server module: can't listen on socket");
-		return -1;
+		throw InitializeException("main server module: can't listen on socket" +
+				errorString(errno));
 	}
 	lzfs_pretty_syslog(LOG_NOTICE,"main server module: listen on %s:%s",ListenHost,ListenPort);
 

src/chunkserver/hddspacemgr.cc
@@ -38,9 +38,11 @@
 #include <unistd.h>
 
 #include "common/cfg.h"
+#include "common/cwrap.h"
 #include "common/crc.h"
 #include "common/datapack.h"
 #include "common/disk_info.h"
+#include "common/exceptions.h"
 #include "common/list.h"
 #include "common/main.h"
 #include "common/massert.h"
@@ -3629,7 +3631,7 @@ void* hdd_folder_scan(void *arg) {
 	}
 	free(fullname);
 	free(destorage);
-//      fprintf(stderr,"hdd space manager: %s: %" PRIu32 " chunks found\n",f->path,f->chunkcount);
+//      lzfs_pretty_syslog(LOG_INFO, "%s: %" PRIu32 " chunks found",f->path,f->chunkcount);
 
 	hdd_testshuffle(f);
 
@@ -3880,7 +3882,6 @@ int hdd_parseline(char *hddcfgline) {
 	uint32_t l,p;
 	int lfd,td;
 	char *pptr;
-	char *lockfname;
 	struct stat sb;
 	folder *f;
 	uint8_t lockneeded;
@@ -3957,7 +3958,7 @@ int hdd_parseline(char *hddcfgline) {
 			} else {
 				uint64_t size = (uint64_t)(fsinfo.f_frsize)*(uint64_t)(fsinfo.f_blocks-(fsinfo.f_bfree-fsinfo.f_bavail));
 				if (limit > size) {
-					lzfs_pretty_syslog(LOG_WARNING,"hdd space manager: space to be left free on '%s' (%" PRIu64 ") is greater than real volume size (%" PRIu64 ") !!!",pptr,limit,size);
+					lzfs_pretty_syslog(LOG_WARNING,"hdd space manager: space to be left free on '%s' (%" PRIu64 ") is greater than real volume size (%" PRIu64 ")",pptr,limit,size);
 				}
 			}
 		}
@@ -3981,48 +3982,46 @@ int hdd_parseline(char *hddcfgline) {
 		}
 	}
 
-	lockfname = (char*)malloc(l+6);
-	passert(lockfname);
-	memcpy(lockfname,pptr,l);
-	memcpy(lockfname+l,".lock",6);
-	lfd = open(lockfname,O_RDWR|O_CREAT|O_TRUNC,0640);
+	std::string dataDir(pptr, l);
+	std::string lockfname = dataDir + ".lock";
+	lfd = open(lockfname.c_str(),O_RDWR|O_CREAT|O_TRUNC,0640);
 	if (lfd<0 && errno==EROFS && td) {
-		free(lockfname);
 		td = 2;
 	} else {
 		if (lfd<0) {
-			lzfs_pretty_errlog(LOG_ERR,"hdd space manager: can't create lock file '%s'",lockfname);
-			free(lockfname);
-			return -1;
+			throw ParseException("can't create lock file " + lockfname + " : " +
+					errorString(errno));
 		}
 		if (lockneeded && lockf(lfd,F_TLOCK,0)<0) {
-			if (errno==EAGAIN) {
-				lzfs_pretty_syslog(LOG_ERR,"hdd space manager: data folder '%s' already locked (used by another process)",pptr);
-			} else {
-				lzfs_pretty_errlog(LOG_NOTICE,"hdd space manager: lockf '%s' error",lockfname);
-			}
-			free(lockfname);
+			int err = errno;
 			close(lfd);
-			return -1;
+			if (errno==EAGAIN) {
+				throw InitializeException(
+						"data folder " + dataDir + " already locked by another process");
+			} else {
+				throw InitializeException("lockf(" + lockfname + ") failed: " + errorString(err));
+			}
 		}
 		if (fstat(lfd,&sb)<0) {
-			lzfs_pretty_errlog(LOG_NOTICE,"hdd space manager: fstat '%s' error",lockfname);
-			free(lockfname);
+			int err = errno;
 			close(lfd);
-			return -1;
+			throw InitializeException("fstat(" + lockfname + ") failed: " + errorString(err));
 		}
-		free(lockfname);
 		if (lockneeded) {
 			zassert(pthread_mutex_lock(&folderlock));
 			for (f=folderhead ; f ; f=f->next) {
 				if (f->devid==sb.st_dev) {
 					if (f->lockinode==sb.st_ino) {
-						lzfs_pretty_syslog(LOG_ERR,"hdd space manager: data folders '%s' and '%s have the same lockfile !!!",pptr,f->path);
+						std::string fPath = f->path;
 						zassert(pthread_mutex_unlock(&folderlock));
 						close(lfd);
-						return -1;
+						throw InitializeException("data folders '" + dataDir + "' and "
+								"'" + fPath + "' have the same lockfile");
 					} else {
-						lzfs_pretty_syslog(LOG_WARNING,"hdd space manager: data folders '%s' and '%s' are on the same physical device (could lead to unexpected behaviours)",pptr,f->path);
+						lzfs_pretty_syslog(LOG_WARNING,
+								"data folders '%s' and '%s' are on the same "
+								"physical device (could lead to unexpected behaviours)",
+								dataDir.c_str(), f->path);
 					}
 				}
 			}
@@ -4123,36 +4122,34 @@ int hdd_parseline(char *hddcfgline) {
 	return 2;
 }
 
-int hdd_folders_reinit(void) {
+static void hdd_folders_reinit(void) {
 	folder *f;
-	FILE *fd;
-	char buff[1000];
-	char *hddfname;
-	int ret,datadef;
+	cstream_t fd;
+	std::string hddfname;
 
 	if (!cfg_isdefined("HDD_CONF_FILENAME")) {
-		hddfname = strdup(ETC_PATH "/mfs/mfshdd.cfg");
-		passert(hddfname);
-		fd = fopen(hddfname,"r");
+		hddfname = ETC_PATH "/mfs/mfshdd.cfg";
+		fd.reset(fopen(hddfname.c_str(),"r"));
 		if (!fd) {
-			free(hddfname);
-			hddfname = strdup(ETC_PATH "/mfshdd.cfg");
-			fd = fopen(hddfname,"r");
+			hddfname = ETC_PATH "/mfshdd.cfg";
+			fd.reset(fopen(hddfname.c_str(),"r"));
 			if (fd) {
 				lzfs_pretty_syslog(LOG_WARNING,"default sysconf path has changed - please move mfshdd.cfg from " ETC_PATH "/ to " ETC_PATH "/mfs/");
+			} else {
+				hddfname = ETC_PATH "/mfs/mfshdd.cfg";
 			}
 		}
 	} else {
-		hddfname = cfg_getstr("HDD_CONF_FILENAME", ETC_PATH "/mfs/mfshdd.cfg");
-		fd = fopen(hddfname,"r");
+		hddfname = cfg_get("HDD_CONF_FILENAME", ETC_PATH "/mfs/mfshdd.cfg");
+		fd.reset(fopen(hddfname.c_str(),"r"));
 	}
 
 	if (!fd) {
-		free(hddfname);
-		return -1;
+		throw InitializeException("can't open hdd config file " + hddfname +": " +
+				strerr(errno) + " - new file can be created using " +
+				ETC_PATH "/mfs/mfshdd.cfg.dist");
 	}
-
-	ret = 0;
+	lzfs_pretty_syslog(LOG_INFO, "hdd configuration file %s opened", hddfname.c_str());
 
 	zassert(pthread_mutex_lock(&folderlock));
 	folderactions = 0; // stop folder actions
@@ -4161,20 +4158,18 @@ int hdd_folders_reinit(void) {
 	}
 	zassert(pthread_mutex_unlock(&folderlock));
 
-	while (fgets(buff,999,fd)) {
+	char buff[1000];
+	while (fgets(buff,999,fd.get())) {
 		buff[999] = 0;
-		if (hdd_parseline(buff)<0) {
-			ret = -1;
-		}
-
+		hdd_parseline(buff);
 	}
-	fclose(fd);
+	fd.reset();
 
 	zassert(pthread_mutex_lock(&folderlock));
-	datadef = 0;
+	bool anyDiskAvailable = false;
 	for (f=folderhead ; f ; f=f->next) {
 		if (f->toremove==0) {
-			datadef = 1;
+			anyDiskAvailable = true;
 			if (f->scanstate==SCST_SCANNEEDED) {
 				syslog(LOG_NOTICE,"hdd space manager: folder %s will be scanned",f->path);
 			} else if (f->scanstate==SCST_SENDNEEDED) {
@@ -4189,14 +4184,9 @@ int hdd_folders_reinit(void) {
 	folderactions = 1; // continue folder actions
 	zassert(pthread_mutex_unlock(&folderlock));
 
-	if (datadef==0) {
-		lzfs_pretty_syslog(LOG_ERR,"hdd space manager: no hdd space defined in %s file",hddfname);
-		ret = -1;
+	if (!anyDiskAvailable) {
+		throw InitializeException("no data paths defined in the " + hddfname + " file");
 	}
-
-	free(hddfname);
-
-	return ret;
 }
 
 void hdd_reload(void) {
@@ -4216,7 +4206,11 @@ void hdd_reload(void) {
 	}
 
 	syslog(LOG_NOTICE,"reloading hdd data ...");
-	hdd_folders_reinit();
+	try {
+		hdd_folders_reinit();
+	} catch (const Exception& ex) {
+		lzfs_pretty_syslog(LOG_ERR, "%s", ex.what());
+	}
 }
 
 int hdd_late_init(void) {
@@ -4252,17 +4246,21 @@ int hdd_init(void) {
 
 	LeaveFreeStr = cfg_getstr("HDD_LEAVE_SPACE_DEFAULT","256MiB");
 	if (hdd_size_parse(LeaveFreeStr,&LeaveFree)<0) {
-		fprintf(stderr,"hdd space manager: HDD_LEAVE_SPACE_DEFAULT parse error - using default (256MiB)\n");
+		lzfs_pretty_syslog(LOG_WARNING,
+				"%s: HDD_LEAVE_SPACE_DEFAULT parse error - using default (256MiB)",
+				cfg_filename().c_str());
 		LeaveFree = 0x10000000;
 	}
 	free(LeaveFreeStr);
 	if (LeaveFree<0x4000000) {
-		fprintf(stderr,"hdd space manager: HDD_LEAVE_SPACE_DEFAULT < chunk size - leaving so small space on hdd is not recommended\n");
+		lzfs_pretty_syslog(LOG_WARNING,
+				"%s: HDD_LEAVE_SPACE_DEFAULT < chunk size - "
+				"leaving so small space on hdd is not recommended",
+				cfg_filename().c_str());
 	}
 
-	if (hdd_folders_reinit()<0) {
-		return -1;
-	}
+	/* this can throw exception*/
+	hdd_folders_reinit();
 
 	zassert(pthread_attr_init(&thattr));
 	zassert(pthread_attr_setstacksize(&thattr,0x100000));
@@ -4270,10 +4268,11 @@ int hdd_init(void) {
 
 	zassert(pthread_mutex_lock(&folderlock));
 	for (f=folderhead ; f ; f=f->next) {
-		fprintf(stderr,"hdd space manager: path to scan: %s\n",f->path);
+		lzfs_pretty_syslog(LOG_INFO, "hdd space manager: path to scan: %s",f->path);
 	}
 	zassert(pthread_mutex_unlock(&folderlock));
-	fprintf(stderr,"hdd space manager: start background hdd scanning (searching for available chunks)\n");
+	lzfs_pretty_syslog(LOG_INFO, "hdd space manager: start background hdd scanning "
+				"(searching for available chunks)");
 
 	HDDTestFreq = cfg_getuint32("HDD_TEST_FREQ",10);
 

src/chunkserver/masterconn.cc
@@ -684,11 +684,11 @@ int masterconn_initconnect(masterconn *eptr) {
 		return -1;
 	}
 	if (status==0) {
-		syslog(LOG_NOTICE,"connected to Master immediately");
+		lzfs_pretty_syslog(LOG_NOTICE,"connected to Master immediately");
 		masterconn_connected(eptr);
 	} else {
 		eptr->mode = CONNECTING;
-		syslog(LOG_NOTICE,"connecting ...");
+		lzfs_pretty_syslog_attempt(LOG_NOTICE,"connecting to Master");
 	}
 	return 0;
 }
@@ -893,7 +893,7 @@ bool masterconn_load_label() {
 	std::string oldLabel = gLabel;
 	gLabel = cfg_getstring("LABEL", kMediaLabelWildcard);
 	if (!isMediaLabelValid(gLabel)) {
-		lzfs_pretty_syslog(LOG_WARNING,"invalid label '%s' !!!", gLabel.c_str());
+		lzfs_pretty_syslog(LOG_WARNING,"invalid label '%s'", gLabel.c_str());
 		return false;
 	}
 	return gLabel != oldLabel;

src/common/cfg.cc
@@ -54,7 +54,7 @@ static int cfg_do_load (void) {
 
 	fd = fopen(cfgfname,"r");
 	if (fd==NULL) {
-		lzfs_pretty_syslog(LOG_ERR,"cannot load config file: %s",cfgfname);
+		lzfs_silent_syslog(LOG_ERR,"can't load config file: %s",cfgfname);
 		return 0;
 	}
 	while (fgets(linebuff,999,fd)!=NULL) {
@@ -123,6 +123,14 @@ int cfg_load (const char *configfname,int _lu) {
 	cfgfname = strdup(configfname);
 
 	return cfg_do_load();
+}
+
+std::string cfg_filename() {
+	if (cfgfname != nullptr) {
+		return cfgfname;
+	} else {
+		return "";
+	}
 }
 
 int cfg_isdefined(const char *name) {

src/common/cfg.h
@@ -30,6 +30,9 @@ int cfg_load (const char *fname,int logundefined);
 int cfg_reload (void);
 void cfg_term (void);
 
+/// Returns the name of the currently loaded config file.
+std::string cfg_filename();
+
 int cfg_isdefined(const char *name);
 
 _CONFIG_MAKE_PROTOTYPE(str,char*);

commit 7580fe65c4caaa057d8ea6479a91fbb0f859ec90

main: Fix wrong message when terminating takes a lot of time
When a daemon needs a lot of time to exit after receiving SIGTERM, the
'xxxxxx stop' command should print something like:

[WARN] about 10 seconds passed and lock .daemon.lock exists (...)...
[WARN] about 20 seconds passed and lock .daemon.lock exists (...)...
[FAIL] about 30 seconds passed and lock .daemon.lock exists (...) - giving up

instead of:

[WARN] about 10 seconds passed and lock .daemon.lock exists (...) - giving up
[WARN] about 20 seconds passed and lock .daemon.lock exists (...) - giving up
[FAIL] about 30 seconds passed and lock .daemon.lock exists (...) - giving up

This commit fixes the bug (introduced in e701e69bac1a4bed448).

Change-Id: Ia78a2b747810ea5998524fdf68583ca07080b727

src/main/main.cc
@@ -868,14 +868,14 @@ FileLock::LockStatus FileLock::wdlock(RunMode runmode, uint32_t timeout) {
 				uint32_t secondsElapsed = l / checksPerSecond;
 				if (secondsElapsed >= timeout) {
 					lzfs_pretty_syslog(LOG_ERR, "about %" PRIu32 " seconds passed and lock %s still "
-								"exists in working directory - giving up",
-								secondsElapsed, name_.c_str());
+							"exists in working directory - giving up",
+							secondsElapsed, name_.c_str());
 					return LockStatus::kFail;
 				}
 				if (l % (10 * checksPerSecond) == 0) {
 					lzfs_pretty_syslog(LOG_WARNING, "about %" PRIu32 " seconds passed and lock %s "
-								"still exists in working directory - giving up",
-								secondsElapsed, name_.c_str());
+							"still exists in working directory...",
+							secondsElapsed, name_.c_str());
 					fflush(stderr);
 				}
 				if (newownerpid!=ownerpid) {