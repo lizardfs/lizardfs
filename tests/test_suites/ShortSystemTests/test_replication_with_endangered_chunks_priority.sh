1bb523364481139d44410a277c0e0a64be11d255
Piotr Sarna <sarna@skytechnology.pl>
2015-02-10 10:46:07 +0100

    master: Prioritize replication of endangered chunks
    This commit introduces a mechanism of prioritizing endangered chunks during
    chunk maintenance. Depending on the config value ENDANGERED_CHUNKS_PRIORITY,
    appropriate percentage of chunks intended for processing would be taken
    from endangered chunks queue. A chunk is added to this queue when it's goal
    is greater than 1, but number of it's valid copies drops to 1.
    
    By default, ENDANGERED_CHUNKS_PRIORITY is set to 0, causing no overhead,
    because endangered chunks will not be collected in any queue.
    
    This commit comes with 3 tests, which check whether lizardfs operates without
    errors if endangered prioritization is turned on.
    
    Closes #244
    
    Change-Id: I1729844c64033dd9743e4723a7c94e47860127ea

---
+++ tests/test_suites/ShortSystemTests/test_replication_with_endangered_chunks_priority.sh
@@ -0,0 +1,51 @@
+timeout_set "1 minute"
+
+# Start an installation with 2 servers labeled 'hdd', 2 labeled 'ssd' and the default goal "ssd ssd"
+USE_RAMDISK=YES \
+	CHUNKSERVERS=4 \
+	CHUNKSERVER_LABELS="0,1:ssd|2,3:hdd" \
+	MASTER_CUSTOM_GOALS="1 default: ssd ssd" \
+	MASTER_EXTRA_CONFIG="CHUNKS_LOOP_TIME = 1`
+			`|ACCEPTABLE_DIFFERENCE = 1.0`
+			`|CHUNKS_WRITE_REP_LIMIT = 5`
+			`|REPLICATIONS_DELAY_INIT = 0`
+			`|ENDANGERED_CHUNKS_PRIORITY = 0.7`
+			`|REPLICATIONS_DELAY_DISCONNECT = 0" \
+	setup_local_empty_lizardfs info
+
+# Leave only one "hdd" and one "ssd" server.
+lizardfs_chunkserver_daemon 1 stop
+lizardfs_chunkserver_daemon 2 stop
+lizardfs_wait_for_ready_chunkservers 2
+
+# Create 20 files. Expect that for each file there are 2 chunk copies.
+FILE_SIZE=1K file-generate "${info[mount0]}"/file{1..20}
+assert_equals 20 $(mfscheckfile "${info[mount0]}"/* | grep 'with 2 copies: *1' | wc -l)
+
+# Stop the chunkserver labeled "ssd" and expect all files to have a chunk in only one copy.
+assert_equals 20 $(find_chunkserver_chunks 0 | wc -l)
+lizardfs_chunkserver_daemon 0 stop
+lizardfs_wait_for_ready_chunkservers 1
+assert_equals 20 $(mfscheckfile "${info[mount0]}"/* | grep 'with 1 copy: *1' | wc -l)
+
+# Add one "hdd" chunkserver. Expect that second copy of each chunk will be created there.
+lizardfs_chunkserver_daemon 2 start
+lizardfs_wait_for_ready_chunkservers 2
+assert_eventually_prints 20 'mfscheckfile "${info[mount0]}"/* | grep "with 2 copies: *1" | wc -l'
+
+# Remove all chunks from the chunkserver "ssd" and bring it back to life.
+find_chunkserver_chunks 0 | xargs -d'\n' rm -f
+assert_equals 0 $(find_chunkserver_chunks 0 | wc -l)
+lizardfs_chunkserver_daemon 0 start
+lizardfs_wait_for_ready_chunkservers 3
+
+# Expect one copy of each chunk to migrate to the "ssd" server.
+assert_eventually_prints 20 'find_chunkserver_chunks 0 | wc -l'
+
+# No chunks should be deleted until we have two "ssd" servers. So let's add one.
+assert_eventually_prints 60 'find_all_chunks | wc -l'
+lizardfs_chunkserver_daemon 1 start
+lizardfs_wait_for_ready_chunkservers 4
+assert_eventually_prints 20 'find_chunkserver_chunks 1 | wc -l'
+assert_eventually_prints 20 'find_chunkserver_chunks 0 | wc -l'
+assert_eventually_prints 40 'find_all_chunks | wc -l'
