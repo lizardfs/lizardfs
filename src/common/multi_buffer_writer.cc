a89867967297574a660c2c02cfd816d22ad98ccd
Piotr Sarna <sarna@skytechnology.pl>
2015-07-16 13:53:31 +0200

    common: Bypass writev usage for mingw
    This commit replaces call to writev() function
    with crude implementation of writing to multiple buffers.
    
    Change-Id: I5b9a87c052a9940220e353d8be3110fdb0f5c63b

--- src/common/multi_buffer_writer.cc
+++ src/common/multi_buffer_writer.cc
@@ -3,6 +3,46 @@
 
 #include <inttypes.h>
 
+#ifdef _WIN32
+#include "common/sockets.h"
+
+ssize_t writev(int fd, const struct iovec *iov, int iovcnt) {
+	ssize_t ret = 0;
+	ssize_t send_ret = 0;
+
+	for (int i = 0; i < iovcnt; ++i) {
+		const struct iovec &buffer = iov[i];
+
+		if (i > 0) { // check if next write would block
+			int poll_ret = tcptopoll(fd, POLLOUT, 0);
+			// If poll failed or next write would block, return partial write
+			// if anything was written already or error if nothing was written yet
+			if (poll_ret <= 0) {
+				ret = ret ? ret : poll_ret;
+				break;
+			}
+		}
+
+		send_ret = tcpsend(fd, buffer.iov_base, buffer.iov_len);
+
+		// If write failed, return partial write or error if nothing was written yet
+		if (send_ret < 0) {
+			ret = ret ? ret : send_ret;
+			break;
+		}
+
+		ret += send_ret;
+
+		// If partial write occurred, return
+		if ((size_t)send_ret != buffer.iov_len) {
+			break;
+		}
+	}
+	return ret;
+}
+
+#endif
+
 void MultiBufferWriter::addBufferToSend(const void* buffer, size_t size) {
 	struct iovec iov;
 	iov.iov_base = (void*)buffer; // remove 'const' as we will pass this iovec to writev
