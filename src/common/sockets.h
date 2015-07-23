231cb5fcee35dd01e4825b67d197383757f2adf5
Hazeman <hazeman@skytechnology.pl>
2015-07-23 13:20:28 +0200

    common: Improve socket library
    This commit adds Windows compatibility to socket library.
    Also there are some improvements to tcppoll and
    option to emulate poll function with select.
    
    Change-Id: Ib79c9816be886019c05e200a3c52a9631ed1ddae

--- src/common/sockets.h
+++ src/common/sockets.h
@@ -1,5 +1,6 @@
 /*
-   Copyright 2005-2010 Jakub Kruszona-Zawadzki, Gemius SA, 2013-2014 EditShare, 2013-2015 Skytechnology sp. z o.o..
+   Copyright 2005-2010 Jakub Kruszona-Zawadzki, Gemius SA, 2013-2014 EditShare, 2013-2015
+   Skytechnology sp. z o.o..
 
    This file was part of MooseFS and is part of LizardFS.
 
@@ -21,43 +22,69 @@
 #include "common/platform.h"
 
 #include <inttypes.h>
-#include <sys/poll.h>
+#ifdef _WIN32
+#include <winsock2.h>
+#else
+#include <poll.h>
+#endif
+
+#ifdef _WIN32
+#define TCPEAGAIN WSAEWOULDBLOCK
+#define TCPEINPROGRESS WSAEWOULDBLOCK
+#else
+#define TCPEAGAIN EAGAIN
+#define TCPEINPROGRESS EINPROGRESS
+#endif
+
+#include <vector>
+
+/* ----------------- SOCKET--------------- */
+
+int socketinit();
+int socketrelease();
 
 /* ----------------- TCP ----------------- */
 
+int tcpgetlasterror();
 int tcpsocket(void);
-int tcpresolve(const char *hostname,const char *service,uint32_t *ip,uint16_t *port,int passiveflag);
+int tcpresolve(const char *hostname, const char *service, uint32_t *ip, uint16_t *port,
+		int passiveflag);
 int tcpnonblock(int sock);
 int tcpsetacceptfilter(int sock);
 int tcpreuseaddr(int sock);
 int tcpnodelay(int sock);
 int tcpaccfhttp(int sock);
 int tcpaccfdata(int sock);
-int tcpnumbind(int sock,uint32_t ip,uint16_t port);
-int tcpstrbind(int sock,const char *hostname,const char *service);
-int tcpnumconnect(int sock,uint32_t ip,uint16_t port);
-int tcpnumtoconnect(int sock,uint32_t ip,uint16_t port,uint32_t msecto);
-int tcpstrconnect(int sock,const char *hostname,const char *service);
-int tcpstrtoconnect(int sock,const char *hostname,const char *service,uint32_t msecto);
+int tcpnumbind(int sock, uint32_t ip, uint16_t port);
+int tcpstrbind(int sock, const char *hostname, const char *service);
+int tcpnumconnect(int sock, uint32_t ip, uint16_t port);
+int tcpnumtoconnect(int sock, uint32_t ip, uint16_t port, uint32_t msecto);
+int tcpstrconnect(int sock, const char *hostname, const char *service);
+int tcpstrtoconnect(int sock, const char *hostname, const char *service, uint32_t msecto);
 int tcpgetstatus(int sock);
-int tcpnumlisten(int sock,uint32_t ip,uint16_t port,uint16_t queue);
-int tcpstrlisten(int sock,const char *hostname,const char *service,uint16_t queue);
+int tcpnumlisten(int sock, uint32_t ip, uint16_t port, uint16_t queue);
+int tcpstrlisten(int sock, const char *hostname, const char *service, uint16_t queue);
 int tcpaccept(int lsock);
-int tcpgetpeer(int sock,uint32_t *ip,uint16_t *port);
-int tcpgetmyaddr(int sock,uint32_t *ip,uint16_t *port);
+int tcpgetpeer(int sock, uint32_t *ip, uint16_t *port);
+int tcpgetmyaddr(int sock, uint32_t *ip, uint16_t *port);
 int tcpclose(int sock);
-int tcptopoll(int sock,int events,uint32_t msecto);
-int32_t tcptoread(int sock,void *buff,uint32_t leng,uint32_t msecto);
-int32_t tcptowrite(int sock,const void *buff,uint32_t leng,uint32_t msecto);
-int tcptoaccept(int sock,uint32_t msecto);
+int tcptopoll(int sock, int events, int msecto);
+int tcppoll(pollfd &pfd, int msecto);
+int tcppoll(std::vector<pollfd> &pfd, int msecto);
+int32_t tcprecv(int sock, void *buff, uint32_t len, int flags = 0);
+int32_t tcpsend(int sock, const void *buff, uint32_t len, int flags = 0);
+int32_t tcptoread(int sock, void *buff, uint32_t leng, uint32_t msecto);
+int32_t tcptowrite(int sock, const void *buff, uint32_t leng, uint32_t msecto);
+int tcptoaccept(int sock, uint32_t msecto);
 
 /* ----------------- UDP ----------------- */
 
 int udpsocket(void);
-int udpresolve(const char *hostname,const char *service,uint32_t *ip,uint16_t *port,int passiveflag);
+int udpresolve(const char *hostname, const char *service, uint32_t *ip, uint16_t *port,
+		int passiveflag);
 int udpnonblock(int sock);
-int udpnumlisten(int sock,uint32_t ip,uint16_t port);
-int udpstrlisten(int sock,const char *hostname,const char *service);
-int udpwrite(int sock,uint32_t ip,uint16_t port,const void *buff,uint16_t leng);
-int udpread(int sock,uint32_t *ip,uint16_t *port,void *buff,uint16_t leng);
+int udpnumlisten(int sock, uint32_t ip, uint16_t port);
+int udpstrlisten(int sock, const char *hostname, const char *service);
+int udpwrite(int sock, uint32_t ip, uint16_t port, const void *buff, uint16_t leng);
+int udpread(int sock, uint32_t *ip, uint16_t *port, void *buff, uint16_t leng);
 int udpclose(int sock);
