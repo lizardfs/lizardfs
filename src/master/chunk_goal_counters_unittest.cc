0ff9244fbcbd6af566eafa33407a4a4007c9ddbf
Piotr Sarna <sarna@skytechnology.pl>
2015-09-07 15:17:17 +0200

    master: Restore unittests for chunk goal counters
    This commit restores unit testing for ChunkGoalCounters class.
    
    Change-Id: I7d58c7f49e3e6bb32f78904a341208bbe2f166d5

--- src/master/chunk_goal_counters_unittest.cc
+++ src/master/chunk_goal_counters_unittest.cc
@@ -20,19 +20,12 @@
 #include "master/chunk_goal_counters.h"
 
 #include <algorithm>
+#include <map>
 
 #include <gtest/gtest.h>
 
 #define EXPECT_FINDS(value, container) \
 	EXPECT_NE(std::find(container.begin(), container.end(), (value)), container.end())
-
-// FIXME
-/*static uint8_t xor2 = goal::xorLevelToGoal(2);
-static uint8_t xor3 = goal::xorLevelToGoal(3);
-static uint8_t xor5 = goal::xorLevelToGoal(5);
-static uint8_t xor9 = goal::xorLevelToGoal(9);
-typedef std::vector<uint8_t> Goals;
-typedef Goals ExpectedGoals;
 
 TEST(ChunkGoalCounters, Add) {
 	ChunkGoalCounters counters;
@@ -58,29 +51,6 @@ TEST(ChunkGoalCounters, Add) {
 	for (auto counter : std::vector<ChunkGoalCounters::GoalCounter>{{1,3}, {2,1}, {3,2}}) {
 		EXPECT_FINDS(counter, counters);
 	}
-}
-
-#define EXPECT_GOAL(expected, goals) do { \
-			ChunkGoalCounters counters; \
-			for (int goal : goals) { \
-				counters.addFile(goal); \
-			} \
-			EXPECT_EQ(expected, counters.highestIdGoal()); \
-		} while(0)
-
-TEST(ChunkGoalCounters, XorAdd) {
-	EXPECT_GOAL(xor2, Goals({1, xor2}));
-	EXPECT_GOAL(xor2, Goals({xor2, 1}));
-	EXPECT_GOAL(3U, Goals({3, xor2}));
-	EXPECT_GOAL(3U, Goals({xor2, 3}));
-	EXPECT_GOAL(2U, Goals({2, xor5}));
-	EXPECT_GOAL(2U, Goals({xor5, 2}));
-	EXPECT_GOAL(xor5, Goals({1, xor5}));
-	EXPECT_GOAL(xor5, Goals({xor5, 1}));
-	EXPECT_GOAL(xor5, Goals({xor2, xor5, xor3}));
-	EXPECT_GOAL(2, Goals({xor2, 2, xor5, 1, xor3, 1}));
-	EXPECT_GOAL(xor5, Goals({xor2, 1, xor5, 1, xor3, 1}));
-	EXPECT_GOAL(4U, Goals({xor2, 4, xor5, 1, xor9, 3}));
 }
 
 TEST(ChunkGoalCounters, Remove) {
@@ -127,42 +97,6 @@ TEST(ChunkGoalCounters, Remove) {
 	EXPECT_FINDS((ChunkGoalCounters::GoalCounter{2,1}), counters);
 }
 
-#define EXPECT_GOAL_AND_REMOVE(expected, goals) do { \
-			ChunkGoalCounters counters; \
-			EXPECT_EQ(goals.size(), expected.size()); \
-			for (size_t indx = 0; indx < goals.size(); ++indx) { \
-				counters.addFile(goals[indx]); \
-				EXPECT_EQ(expected[indx], counters.highestIdGoal()); \
-			} \
-			for (ssize_t indx = goals.size() - 1; indx >= 0; --indx) { \
-				EXPECT_EQ(expected[indx], counters.highestIdGoal()); \
-				counters.removeFile(goals[indx]); \
-			} \
-			EXPECT_EQ(0, counters.highestIdGoal()); \
-		} while(0)*/
-
-/*
- * This test first adds goals from the `goals' vector and verifies that the goal is equal to
- * the one provided in `expected' vector. Then it iterates backwards through the vectors removing
- * goals and verifying the result. At the very end goal should be 0.
- */
-/*TEST(ChunkGoalCounters, XorRemove) {
-	EXPECT_GOAL_AND_REMOVE(ExpectedGoals({1, xor2}), Goals({1, xor2}));
-	EXPECT_GOAL_AND_REMOVE(ExpectedGoals({xor2, xor2}), Goals({xor2, 1}));
-	EXPECT_GOAL_AND_REMOVE(ExpectedGoals({3, 3}), Goals({3, xor2}));
-	EXPECT_GOAL_AND_REMOVE(ExpectedGoals({xor2, 3}), Goals({xor2, 3}));
-	EXPECT_GOAL_AND_REMOVE(ExpectedGoals({2, 2}), Goals({2, xor5}));
-	EXPECT_GOAL_AND_REMOVE(ExpectedGoals({xor5, xor5}), Goals({xor5, 1}));
-	EXPECT_GOAL_AND_REMOVE(ExpectedGoals({1, xor5}), Goals({1, xor5}));
-	EXPECT_GOAL_AND_REMOVE(ExpectedGoals({xor5, 2}), Goals({xor5, 2}));
-	EXPECT_GOAL_AND_REMOVE(ExpectedGoals({xor2, xor5, xor5}), Goals({xor2, xor5, xor3}));
-	EXPECT_GOAL_AND_REMOVE(ExpectedGoals({1, xor2, 2, 2, 2, 2, 2}),
-			Goals({1, xor2, 2, xor5, 1, xor3, 1}));
-	EXPECT_GOAL_AND_REMOVE(ExpectedGoals({1, xor2, xor2, xor5, xor5, xor5, xor5}),
-				Goals({1, xor2, 1, xor5, 1, xor3, 1}));
-	EXPECT_GOAL_AND_REMOVE(ExpectedGoals({xor2, 4U, 4U, 4U, 4U, 4U}),
-			Goals({xor2, 4, xor5, 1, xor9, 3}));
-}
 TEST(ChunkGoalCounters, Change) {
 	ChunkGoalCounters counters;
 	counters.addFile(1);
@@ -189,38 +123,6 @@ TEST(ChunkGoalCounters, Change) {
 	for (auto counter : std::vector<ChunkGoalCounters::GoalCounter>{{3,1}, {2,1}, {1,1}}) {
 		EXPECT_FINDS(counter, counters);
 	}
-}
-#define EXPECT_GOAL_AND_CHANGE(expected, goals, changes) do { \
-	ChunkGoalCounters counters; \
-	EXPECT_EQ(goals.size() + changes.size(), expected.size()); \
-	for (size_t indx = 0; indx < goals.size(); ++indx) { \
-		counters.addFile(goals[indx]); \
-		EXPECT_EQ(expected[indx], counters.highestIdGoal()); \
-	} \
-	for (size_t indx = 0; indx < changes.size(); ++indx) { \
-		counters.changeFileGoal(changes[indx].first, changes[indx].second); \
-		EXPECT_EQ(expected[indx + goals.size()], counters.highestIdGoal()); \
-	} \
-} while(0)*/
-
-/*
- * This test first adds goals from the `goals' vector and verifies that the goal is equal to
- * the one provided in `expected' vector (just like in XorRemove).
- * Then it iterates through `changes' and changes the goal from `change.first' to `change.second'
- * (change is std::pair). `expected' contains expected goals after each operation
- * (i.e. `addFile's and `changeFile's collectively).
- */
-/*TEST(ChunkGoalCounters, XorChange) {
-	typedef std::vector<std::pair<uint8_t, uint8_t>> Changes;
-	EXPECT_GOAL_AND_CHANGE(ExpectedGoals({1, xor2, 3, xor3, xor5, xor3}), Goals({1, xor2}),
-			Changes({{1, 3}, {3, xor3}, {xor2, xor5}, {xor5, 1}}));
-	EXPECT_GOAL_AND_CHANGE(ExpectedGoals({xor5, xor5, 3, 3, xor9}), Goals({xor5, 1}),
-				Changes({{1, 3}, {xor5, xor9}, {3, 1}}));
-	EXPECT_GOAL_AND_CHANGE(ExpectedGoals({xor5, 2, 3, 3, xor9}), Goals({xor5, 2}),
-				Changes({{2, 3}, {xor5, xor9}, {3, 1}}));
-	EXPECT_GOAL_AND_CHANGE(ExpectedGoals({xor2, 4, 4, 4, 4, 4, 5, 5, 5, 5, xor5, xor5}),
-			Goals({xor2, 4, xor5, 1, xor9, 3}),
-			Changes({{4, 5}, {xor2, xor5}, {xor9, xor5}, {3, 1}, {5, 1}, {xor5, xor2}}));
 }
 
 TEST(ChunkGoalCounters, LotsOfGoals) {
@@ -253,4 +155,4 @@ TEST(ChunkGoalCounters, LotsOfGoals) {
 	}
 
 	EXPECT_EQ(counters.size(), 0U);
-}*/
+}
