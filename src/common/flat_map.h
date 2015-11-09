738521a6f974a382980fa7ad3ef19f11edd785db
Wojciech Donderowicz <jotek@skytechnology.pl>
2015-11-09 13:27:56 +0100

    common: Add implementation of flat_map
    This commit introduces flat map implementation built on the top of flat_set.
    This data structure provides map-like API, while keeping elements in a flat
    container, e.g. std::vector.
    
    Change-Id: Ie5fca711ac457ec4606f6b758e6ec343fbf36231

---
+++ src/common/flat_map.h
@@ -0,0 +1,429 @@
+/*
+   Copyright 2015 Skytechnology sp. z o.o.
+
+   This file is part of LizardFS.
+
+   LizardFS is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, version 3.
+
+   LizardFS is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with LizardFS. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "common/platform.h"
+
+#include <algorithm>
+#include <cassert>
+#include <functional>
+#include <stdexcept>
+#include <type_traits>
+#include <vector>
+
+#include "flat_set.h"
+
+/*!
+ * Memory-efficient container - flat_map.
+ * It has interface as close as possible to std::map.
+ * It is possible to pass other container (with vector-like interface)
+ * to this template as a parameter - they will be used for storage.
+ * Time complexity for insert operation is O(n), for find operations it is O(log n).
+ *
+ */
+template <typename Key, typename T, typename C = std::vector<std::pair<Key, T>>,
+	class Compare = std::less<Key>>
+class flat_map {
+private:
+	class internal_compare;
+
+public:
+	typedef Key key_type;
+	typedef T mapped_type;
+	typedef Compare key_compare;
+	typedef internal_compare value_compare;
+	typedef C container_type;
+	typedef std::pair<key_type, mapped_type> value_type;
+
+	typedef typename C::allocator_type allocator_type;
+	typedef typename container_type::size_type size_type;
+	typedef typename container_type::difference_type difference_type;
+	typedef value_type& reference;
+	typedef const value_type& const_reference;
+
+	typedef flat_set<value_type, container_type, value_compare> set_type;
+
+	typedef typename container_type::pointer pointer;
+	typedef typename container_type::const_pointer const_pointer;
+
+	typedef typename set_type::iterator iterator;
+	typedef typename set_type::const_iterator const_iterator;
+	typedef typename set_type::reverse_iterator reverse_iterator;
+	typedef typename set_type::const_reverse_iterator const_reverse_iterator;
+
+	flat_map() noexcept(std::is_nothrow_constructible<set_type, value_compare&&>::value
+			&& std::is_nothrow_constructible<value_compare>::value)
+		: container_(value_compare()) {
+	}
+
+	explicit flat_map(const key_compare &comp) noexcept(
+			std::is_nothrow_constructible<set_type, value_compare &&>::value
+			&& std::is_nothrow_constructible<value_compare, const key_compare &>::value)
+		: container_(value_compare(comp)) {
+	}
+
+	explicit flat_map(container_type &&container) noexcept(
+			std::is_nothrow_constructible<set_type, container_type &&>::value)
+		: container_(std::move(container)) {
+	}
+
+	/*!
+	 * Construct flat map with the contents of the range [first, last).
+	 * \param sorted - Is the range sorted? In this case no duplicates are allowed.
+	 *                 This makes construction O(n) instead of O(nlogn).
+	 */
+	template <class InputIt>
+	flat_map(InputIt first, InputIt last, bool sorted = false, const Compare &comp = Compare())
+		noexcept(std::is_nothrow_constructible<
+				set_type, InputIt, InputIt, bool, value_compare &&>::value
+			&& std::is_nothrow_constructible<value_compare, Compare>::value)
+		: container_(first, last, sorted, value_compare(comp)) {
+	}
+
+	flat_map(const flat_map &other) noexcept(
+			std::is_nothrow_constructible<set_type, const set_type &>::value)
+		: container_(other.container_){};
+
+	flat_map(flat_map &&other) noexcept(std::is_nothrow_constructible<set_type, set_type &&>::value)
+		: container_(std::move(other.container_)) {
+	}
+
+	/*!
+	 * Construct flat map based on initializer list.
+	 * \param sorted - Is the initializer list sorted? In this case no duplicates are allowed.
+	 *                 This makes construction O(n) instead of O(nlogn).
+	 */
+	flat_map(std::initializer_list<value_type> init, bool sorted = false,
+			const Compare& comp = Compare())
+		: container_(init, sorted, value_compare(comp)) {
+	}
+
+	flat_map &operator=(const flat_map &other) {
+		container_ = other.container_;
+		return *this;
+	}
+
+	flat_map &operator=(flat_map &&other) {
+		container_ = std::move(other.container_);
+		return *this;
+	}
+
+	flat_map& operator=(std::initializer_list<value_type> ilist) {
+		container_ = ilist;
+		return *this;
+	}
+
+	void swap(flat_map &other) {
+		std::swap(*this, other);
+	}
+
+	allocator_type get_allocator() const noexcept {
+		return container_.get_allocator();
+	}
+
+	mapped_type &at(const key_type &key) {
+		auto it = lower_bound(key);
+		if (it != container_.end() && !less_than(key, it->first)) {
+			return it->second;
+		}
+
+		throw std::out_of_range("key not found");
+	}
+
+	const mapped_type &at(const key_type &key) const {
+		auto it = lower_bound(key);
+		if (it != container_.end() && !less_than(key, it->first)) {
+			return it->second;
+		}
+
+		throw std::out_of_range("key not found");
+	}
+
+	mapped_type &operator[](const key_type &key) {
+		auto it = lower_bound(key);
+		if (it != container_.end() && !less_than(key, it->first)) {
+			return it->second;
+		}
+
+		const auto &ret = container_.insert(it, {key, mapped_type()});
+		return ret->second;
+	}
+
+	mapped_type &operator[](const key_type &&key) {
+		auto it = lower_bound(key);
+		if (it != container_.end() && !less_than(key, it->first)) {
+			return it->second;
+		}
+
+		const auto &ret = container_.insert(it, {std::move(key), mapped_type()});
+		return ret->second;
+	}
+
+	iterator erase(iterator pos) {
+		return container_.erase(pos);
+	}
+
+	iterator erase(iterator first, iterator last) {
+		return container_.erase(first, last);
+	}
+
+	size_type erase(const key_type& key) {
+		iterator it = lower_bound(key);
+		if (it != container_.end() && !less_than(key, it->first)) {
+			container_.erase(it);
+			return 1;
+		} else  {
+			return 0;
+		}
+	}
+
+	std::pair<iterator, bool> insert(const value_type &value) {
+		return container_.insert(value);
+	}
+
+	std::pair<iterator, bool> insert(value_type &&value) {
+		return container_.insert(std::move(value));
+	}
+
+	iterator insert(iterator hint, const value_type &value) {
+		return container_.insert(hint, value);
+	}
+
+	iterator insert(iterator hint, value_type &&value) {
+		return container_.insert(hint, std::move(value));
+	}
+
+	iterator begin() noexcept {
+		return container_.begin();
+	}
+
+	const_iterator begin() const noexcept {
+		return container_.begin();
+	}
+
+	const_iterator cbegin() const noexcept {
+		return container_.cbegin();
+	}
+
+	iterator end() noexcept {
+		return container_.end();
+	}
+
+	const_iterator end() const noexcept {
+		return container_.end();
+	}
+
+	const_iterator cend() const noexcept {
+		return container_.cend();
+	}
+
+	iterator rbegin() noexcept {
+		return container_.rbegin();
+	}
+
+	const_iterator rbegin() const noexcept {
+		return container_.rbegin();
+	}
+
+	const_iterator crbegin() const noexcept {
+		return container_.crbegin();
+	}
+
+	iterator rend() noexcept {
+		return container_.rend();
+	}
+
+	const_iterator rend() const noexcept {
+		return container_.rend();
+	}
+
+	const_iterator crend() const noexcept {
+		return container_.crend();
+	}
+
+	size_type size() const noexcept {
+		return container_.size();
+	}
+
+	container_type& data() noexcept {
+		return container_.data();
+	}
+
+	const container_type& data() const noexcept {
+		return container_.data();
+	}
+
+	size_type max_size() const noexcept {
+		return container_.max_size();
+	}
+
+	bool full() const noexcept {
+		return size() == max_size();
+	}
+
+	void clear() {
+		container_.clear();
+	}
+
+	bool empty() const noexcept {
+		return container_.empty();
+	}
+
+	// Lookup
+	size_type count(const key_type &key) const {
+		return find(key) != end();
+	}
+
+	/*!
+	 * Returns an iterator pointing to the first element in the map
+	 * that is equal to x.
+	 * The map must be at least partially ordered, i.e. partitioned
+	 * with respect to the expression custom_less(element, value).
+	 * \param custom_less - should be equivalent to:
+	 *                      bool custom_less(const K &a, const K &b);
+	 */
+	template<typename K, typename CustomLess>
+	iterator find(const K &key, CustomLess custom_less) {
+		auto it = container_.lower_bound(key,
+			[custom_less](const value_type &a, const key_type &b) {
+				return custom_less(a.first, b);
+			});
+
+		if (it != container_.end() && !custom_less(key, it->first)) {
+			return it;
+		}
+
+		return container_.end();
+	}
+
+	/*!
+	 * Returns an iterator pointing to the first element in the map
+	 * that is equal to x.
+	 * The map must be at least partially ordered, i.e. partitioned
+	 * with respect to the expression custom_less(element, value).
+	 * \param custom_less - should be equivalent to:
+	 *                      bool custom_less(const K &a, const K &b);
+	 */
+	template<typename K, typename CustomLess>
+	const_iterator find(const K &key, CustomLess custom_less) const {
+		auto it = container_.lower_bound(key,
+			[custom_less](const value_type &a, const key_type &b) {
+				return custom_less(a.first, b);
+			});
+
+		if (it != container_.end() && !custom_less(key, it->first)) {
+			return it;
+		}
+
+		return container_.end();
+	}
+
+	iterator find(const key_type &key) {
+		return find(key, container_.value_comp());
+	}
+
+	const_iterator find(const key_type &key) const {
+		return find(key, container_.value_comp());
+	}
+
+	iterator lower_bound(const key_type &key) {
+		return container_.lower_bound(key, container_.value_comp());
+	}
+
+	const_iterator lower_bound(const key_type &key) const {
+		return container_.lower_bound(key, container_.value_comp());
+	}
+
+	iterator upper_bound(const key_type &key) {
+		return container_.upper_bound(key, container_.value_comp());
+	}
+
+	const_iterator upper_bound(const key_type &key) const {
+		return container_.upper_bound(key, container_.value_comp());
+	}
+
+
+	// Observers
+	const key_compare &key_comp() const noexcept {
+		return static_cast<Compare&>(container_.value_comp());
+	}
+
+	const value_compare &value_comp() const noexcept {
+		return container_.value_comp();
+	}
+
+private:
+	class internal_compare : public Compare {
+	public:
+		internal_compare() noexcept(std::is_nothrow_constructible<Compare>::value) : Compare() {
+		}
+		internal_compare(const internal_compare &other) noexcept(
+				std::is_nothrow_constructible<Compare, const Compare &>::value)
+			: Compare(other) {
+		}
+		internal_compare(internal_compare &&other) noexcept(
+				std::is_nothrow_constructible<Compare, Compare &&>::value)
+			: Compare(std::move(other)) {
+		}
+		explicit internal_compare(Compare comp) noexcept(
+				std::is_nothrow_constructible<Compare, Compare &&>::value)
+			: Compare(std::move(comp)) {
+		}
+
+		internal_compare &operator=(const internal_compare &other) {
+			Compare::operator=(other);
+			return *this;
+		}
+		internal_compare &operator=(internal_compare &&other) {
+			Compare::operator=(std::move(other));
+			return *this;
+		}
+
+		using Compare::operator();
+
+		bool operator() (const key_type &a, const value_type &b) const {
+			return Compare::operator()(a, b.first);
+		}
+
+		bool operator() (const value_type &a, const key_type &b) const {
+			return Compare::operator()(a.first, b);
+		}
+
+		bool operator() (const value_type &a, const value_type &b) const {
+			return Compare::operator()(a.first, b.first);
+		}
+	};
+
+	bool less_than(const key_type &a, const key_type &b) const {
+		return container_.value_comp()(a, b);
+	}
+
+	set_type container_;
+};
+
+
+template <typename T, typename C, class Compare>
+bool operator==(const flat_map<T,C,Compare>& a, const flat_map<T,C,Compare>& b) {
+	return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
+}
+
+template <typename T, typename C, class Compare>
+bool operator!=(const flat_map<T,C,Compare>& a, const flat_map<T,C,Compare>& b) {
+	return a.size() != b.size() || !std::equal(a.begin(), a.end(), b.begin());
+}
---
+++ src/common/flat_map_unittest.cc
@@ -0,0 +1,369 @@
+/*
+   Copyright 2015 Skytechnology sp. z o.o.
+
+   This file is part of LizardFS.
+
+   LizardFS is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, version 3.
+
+   LizardFS is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with LizardFS. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "flat_map.h"
+#include "common/platform.h"
+
+#include <cassert>
+#include <gtest/gtest.h>
+#include <iterator>
+#include <string>
+
+void fill_map(flat_map<int, int> &m) {
+	for (int i = 0; i < 10; ++i) {
+		m[i] = i + 100;
+	}
+}
+
+TEST(FlatMap, Constructors) {
+	flat_map<int, int> a;
+	fill_map(a);
+
+	std::vector<std::pair<int, int>> v = {{1, 1}, {2, 2}, {3, 3}, {4, 4}};
+
+	flat_map<int, int> d(v.begin(), v.end(), true);
+	for (const auto& k : d) {
+		ASSERT_EQ(k.first, k.second);
+	}
+	ASSERT_EQ(d.size(), 4U);
+
+	flat_map<int, int> b(a);
+	flat_map<int, int> c(std::move(a));
+	a = flat_map<int, int>{{4, 16}, {5, 25}, {6, 36}};
+
+	ASSERT_EQ(b.size(), 10U);
+	ASSERT_EQ(c.size(), 10U);
+	ASSERT_EQ(b.data(), c.data());
+	ASSERT_EQ(b, c);
+	ASSERT_TRUE(b != a);
+	ASSERT_EQ(a.size(), 3U);
+}
+
+TEST(FlatMap, Swap) {
+	flat_map<int, int> a, b;
+	fill_map(a);
+
+	b.insert({1, 1});
+	b.insert({11, 11});
+	b.insert({111, 111});
+
+	a.swap(b);
+
+	ASSERT_EQ(b.size(), 10U);
+
+	for (int i = 0; i < 10; i++) {
+		ASSERT_NE(b.find(i), b.end());
+	}
+
+	ASSERT_EQ(a.size(), 3U);
+}
+
+TEST(FlatMap, InsertErase) {
+	flat_map<int, int> m;
+	m.insert({1, 0});
+	m[2] = 2;
+	m[4] = 0;
+	m[4] = 0;
+	m[3] = 4;
+	m[1] = 5;
+	m[9] = 0;
+	m[4] = 0;
+	m[0] = 5;
+	m[4] = 0;
+	m[5] = 6;
+	m[4] = 8;
+	m[8] = 7;
+	m[4] = 0;
+	m[7] = 9;
+	m[7] = 0;
+	m[3] = 2;
+	m[6] = 1;
+	m[4] = 0;
+	m[0] = 4;
+
+	int i = 0;
+	for (auto it = m.begin(); it != m.end(); it++) {
+		ASSERT_EQ(it->first, i++);
+	}
+	ASSERT_EQ(m.size(), 10U);
+
+	ASSERT_TRUE(m.erase(1));
+	auto it = m.erase(m.find(0));
+	ASSERT_EQ(it->first, 2);
+
+	i = 2;
+	for (auto it = m.begin(); it != m.end(); it++) {
+		ASSERT_EQ(it->first, i++);
+	}
+	ASSERT_EQ(m.size(), 8U);
+
+	m[0] = m[3];
+	ASSERT_EQ(m.size(), 9U);
+	m[3] = m[1];
+	ASSERT_EQ(m.size(), 10U);
+
+	for (int i = 0; i < 10; ++i) {
+		ASSERT_NO_THROW(m.at(i));
+	}
+	ASSERT_THROW(m.at(-1), std::out_of_range);
+	ASSERT_THROW(m.at(10), std::out_of_range);
+
+	flat_map<char, int> ctoi;
+
+	ctoi['e'] = 1;
+	ctoi['b'] = 4;
+	ctoi['c'] = 5;
+	ctoi['d'] = 9;
+	ctoi['a'] = 91;
+	ctoi['a'] = 1;
+	ctoi['a'] = 6;
+	ctoi['a'] = 1;
+	ctoi['f'] = 5;
+	ctoi['a'] = 1;
+	ctoi['g'] = 3;
+	ctoi['g'] = 1;
+	ctoi['e'] = 8;
+	ctoi['a'] = 1;
+	ctoi['a'] = 3;
+	ctoi['d'] = 1;
+	ctoi['a'] = 1;
+	ctoi['a'] = 12;
+	ctoi['r'] = 1;
+	ctoi['a'] = 54;
+	ctoi['x'] = 1;
+	ctoi['a'] = 222;
+
+	EXPECT_EQ(ctoi['a'], 222);
+	EXPECT_EQ(ctoi['b'], 4);
+	EXPECT_EQ(ctoi['c'], 5);
+	EXPECT_EQ(ctoi['d'], 1);
+	EXPECT_EQ(ctoi['e'], 8);
+	EXPECT_EQ(ctoi['f'], 5);
+	EXPECT_EQ(ctoi['g'], 1);
+	EXPECT_EQ(ctoi['r'], 1);
+	EXPECT_EQ(ctoi['x'], 1);
+	ASSERT_EQ(ctoi.size(), 9U);
+}
+
+TEST(FlatMap, At) {
+	flat_map<std::string, int> stoi;
+
+	stoi["heimerdinger"] = 8;
+	stoi["miss fortune"] = 2;
+	stoi["diana"] = 15;
+	stoi["evelyn"] = 3;
+	stoi["lissandra"] = 5;
+	stoi["fiora"] = 2;
+	stoi["gnar"] = 8;
+	stoi["braum"] = 7;
+	stoi["cassiopeia"] = 9;
+	stoi["annie"] = 4;
+	stoi["irelia"] = 9;
+	stoi["jayce"] = 2;
+
+	EXPECT_EQ(stoi.at("annie"), 4);
+	EXPECT_EQ(stoi.at("braum"), 7);
+	EXPECT_EQ(stoi.at("cassiopeia"), 9);
+	EXPECT_EQ(stoi.at("diana"), 15);
+	EXPECT_EQ(stoi.at("evelyn"), 3);
+	EXPECT_EQ(stoi.at("fiora"), 2);
+	EXPECT_EQ(stoi.at("gnar"), 8);
+	EXPECT_EQ(stoi.at("heimerdinger"), 8);
+	EXPECT_EQ(stoi.at("irelia"), 9);
+	EXPECT_EQ(stoi.at("jayce"), 2);
+	EXPECT_EQ(stoi.at("lissandra"), 5);
+	EXPECT_EQ(stoi.at("miss fortune"), 2);
+
+	ASSERT_THROW(stoi.at("Nunu"), std::out_of_range);
+	ASSERT_THROW(stoi.at("Orianna"), std::out_of_range);
+	ASSERT_THROW(stoi.at("Annie"), std::out_of_range);
+}
+
+TEST(FlatMap, Erase) {
+	flat_map<std::string, int> stoi;
+
+	stoi["heimerdinger"] = 8;
+	stoi["miss fortune"] = 2;
+	stoi["diana"] = 15;
+	stoi["evelyn"] = 3;
+	stoi["lissandra"] = 5;
+	stoi["fiora"] = 2;
+	stoi["gnar"] = 8;
+	stoi["braum"] = 7;
+	stoi["cassiopeia"] = 9;
+	stoi["annie"] = 4;
+	stoi["irelia"] = 9;
+	stoi["jayce"] = 2;
+
+	EXPECT_EQ(stoi.at("annie"), 4);
+	EXPECT_EQ(stoi.at("braum"), 7);
+	EXPECT_EQ(stoi.at("cassiopeia"), 9);
+	EXPECT_EQ(stoi.at("diana"), 15);
+	EXPECT_EQ(stoi.at("evelyn"), 3);
+	EXPECT_EQ(stoi.at("fiora"), 2);
+	EXPECT_EQ(stoi.at("gnar"), 8);
+	EXPECT_EQ(stoi.at("heimerdinger"), 8);
+	EXPECT_EQ(stoi.at("irelia"), 9);
+	EXPECT_EQ(stoi.at("jayce"), 2);
+	EXPECT_EQ(stoi.at("lissandra"), 5);
+	EXPECT_EQ(stoi.at("miss fortune"), 2);
+
+	ASSERT_THROW(stoi.at("Nunu"), std::out_of_range);
+	ASSERT_THROW(stoi.at("Orianna"), std::out_of_range);
+	ASSERT_THROW(stoi.at("Annie"), std::out_of_range);
+
+	stoi.erase("heimerdinger");
+	stoi.erase("braum");
+	stoi.erase("heimerdinger");
+
+	EXPECT_EQ(stoi.at("annie"), 4);
+	EXPECT_EQ(stoi.at("cassiopeia"), 9);
+	EXPECT_EQ(stoi.at("diana"), 15);
+	EXPECT_EQ(stoi.at("evelyn"), 3);
+	EXPECT_EQ(stoi.at("fiora"), 2);
+	EXPECT_EQ(stoi.at("gnar"), 8);
+	EXPECT_EQ(stoi.at("irelia"), 9);
+	EXPECT_EQ(stoi.at("jayce"), 2);
+	EXPECT_EQ(stoi.at("lissandra"), 5);
+	EXPECT_EQ(stoi.at("miss fortune"), 2);
+
+	ASSERT_THROW(stoi.at("Nunu"), std::out_of_range);
+	ASSERT_THROW(stoi.at("Orianna"), std::out_of_range);
+	ASSERT_THROW(stoi.at("Annie"), std::out_of_range);
+	ASSERT_THROW(stoi.at("heimerdinger"), std::out_of_range);
+	ASSERT_THROW(stoi.at("braum"), std::out_of_range);
+
+	stoi.erase(stoi.begin());
+
+	EXPECT_EQ(stoi.at("cassiopeia"), 9);
+	EXPECT_EQ(stoi.at("diana"), 15);
+	EXPECT_EQ(stoi.at("evelyn"), 3);
+	EXPECT_EQ(stoi.at("fiora"), 2);
+	EXPECT_EQ(stoi.at("gnar"), 8);
+	EXPECT_EQ(stoi.at("irelia"), 9);
+	EXPECT_EQ(stoi.at("jayce"), 2);
+	EXPECT_EQ(stoi.at("lissandra"), 5);
+	EXPECT_EQ(stoi.at("miss fortune"), 2);
+
+	ASSERT_THROW(stoi.at("Nunu"), std::out_of_range);
+	ASSERT_THROW(stoi.at("Orianna"), std::out_of_range);
+	ASSERT_THROW(stoi.at("Annie"), std::out_of_range);
+	ASSERT_THROW(stoi.at("heimerdinger"), std::out_of_range);
+	ASSERT_THROW(stoi.at("braum"), std::out_of_range);
+	ASSERT_THROW(stoi.at("annie"), std::out_of_range);
+
+	auto first = stoi.find("evelyn");
+	auto last = stoi.find("jayce");
+
+	stoi.erase(first, last);
+
+	EXPECT_EQ(stoi.at("cassiopeia"), 9);
+	EXPECT_EQ(stoi.at("diana"), 15);
+	EXPECT_EQ(stoi.at("jayce"), 2);
+	EXPECT_EQ(stoi.at("lissandra"), 5);
+	EXPECT_EQ(stoi.at("miss fortune"), 2);
+
+	ASSERT_THROW(stoi.at("Nunu"), std::out_of_range);
+	ASSERT_THROW(stoi.at("Orianna"), std::out_of_range);
+	ASSERT_THROW(stoi.at("Annie"), std::out_of_range);
+	ASSERT_THROW(stoi.at("heimerdinger"), std::out_of_range);
+	ASSERT_THROW(stoi.at("braum"), std::out_of_range);
+	ASSERT_THROW(stoi.at("annie"), std::out_of_range);
+	ASSERT_THROW(stoi.at("evelyn"), std::out_of_range);
+	ASSERT_THROW(stoi.at("fiora"), std::out_of_range);
+	ASSERT_THROW(stoi.at("gnar"), std::out_of_range);
+	ASSERT_THROW(stoi.at("irelia"), std::out_of_range);
+}
+
+TEST(FlatMap, Count) {
+	flat_map<std::string, std::string> stos;
+
+	stos["ilidan"] = "stormrage";
+	stos["malfurion"] = "stormrage";
+	stos["sylvanas"] = "windrunner";
+	stos["grommash"] = "hellscream";
+
+	EXPECT_EQ(stos.count("ilidan"), 1U);
+	EXPECT_EQ(stos.count("malfurion"), 1U);
+	EXPECT_EQ(stos.count("arthas"), 0U);
+
+}
+
+class CCmp {
+public:
+	bool operator()(const std::string &a, const std::string &b) const {
+		return b < a;
+	}
+};
+
+TEST(FlatMap, CustomCompare) {
+	flat_map<std::string,
+	std::string,
+	std::vector<std::pair<std::string,std::string>>,
+	CCmp>
+		stos{CCmp()};
+
+	stos["ilidan"] = "stormrage";
+	stos["malfurion"] = "stormrage";
+	stos["sylvanas"] = "windrunner";
+	stos["grommash"] = "hellscream";
+	EXPECT_EQ(stos.begin()->first, "sylvanas");
+}
+
+TEST(FlatMap, Find) {
+	flat_map<std::string,
+	std::string,
+	std::vector<std::pair<std::string,std::string>>,
+	CCmp>
+		stos{CCmp()};
+
+	stos["ilidan"] = "stormrage";
+	stos["malfurion"] = "stormrage";
+	stos["sylvanas"] = "windrunner";
+	stos["grommash"] = "hellscream";
+
+	EXPECT_EQ(stos.find("ilidan")->second, "stormrage");
+	EXPECT_EQ(stos.find("sylvanas")->second, "windrunner");
+	EXPECT_EQ(stos.find("malfurion")->second, "stormrage");
+	EXPECT_EQ(stos.find("grommash")->second, "hellscream");
+	EXPECT_EQ(stos.find("thrall"), stos.end());
+}
+
+TEST(FlatMap, LowerBound) {
+	flat_map<std::string, int> stoi;
+
+	stoi["heimerdinger"] = 8;
+	stoi["miss fortune"] = 2;
+	stoi["diana"] = 15;
+	stoi["evelyn"] = 3;
+	stoi["lissandra"] = 5;
+	stoi["fiora"] = 2;
+	stoi["gnar"] = 8;
+	stoi["braum"] = 7;
+	stoi["cassiopeia"] = 9;
+	stoi["annie"] = 4;
+	stoi["irelia"] = 9;
+	stoi["jayce"] = 2;
+
+	auto lb = stoi.lower_bound("irelia");
+	EXPECT_EQ(lb->first, "irelia");
+
+	lb = stoi.lower_bound("izaaaa");
+	EXPECT_EQ(lb->second, 2);
+
+}
