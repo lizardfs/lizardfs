6d190e4a781b3cdc7f4cd4030bc9925a4a7cd275
skytechnologybot <skytechnologybot@skytechnology.pl>
2016-03-11 15:16:02 +0100

    debian: Enable creating packages with systemd
    This commit enables creating packages with systemd scripts
    instead of /etc/init.d ones.
    
    Change-Id: I6412aa4139741f4efa43be6c8e39d5a2165bc74c

--- create-deb-package.sh
+++ create-deb-package.sh
@@ -6,6 +6,16 @@ output_dir=$(pwd)
 source_dir=$(dirname "$0")
 working_dir=/tmp/lizardfs_deb_working_directory
 
+os_release="$(lsb_release -si)/$(lsb_release -sr)"
+
+# Systemd is added by default, except for the following systems
+case "$os_release" in
+  Debian*/7*)  use_systemd=0 ;;
+  Ubuntu*/12*) use_systemd=0 ;;
+  Ubuntu*/14*) use_systemd=0 ;;
+  *) use_systemd=1 ;;
+esac
+
 # Create an empty working directory and clone sources there to make
 # sure there are no additional files included in the source package
 rm -rf "$working_dir"
@@ -14,6 +24,10 @@ git clone "$source_dir" "$working_dir/lizardfs"
 
 # Build packages.
 cd "$working_dir/lizardfs"
+
+# Move service files to debian/
+cp rpm/service-files/* debian/
+
 if [[ ${BUILD_NUMBER:-} && ${OFFICIAL_RELEASE:-} == "false" ]] ; then
 	# Jenkins has called us. Add build number to the package version
 	# and add information about commit to changelog.
@@ -23,7 +37,12 @@ if [[ ${BUILD_NUMBER:-} && ${OFFICIAL_RELEASE:-} == "false" ]] ; then
 	dch -D "unstable" -a "commit: ${GIT_COMMIT:-}"
 	dch -D "unstable" -a "refspec: ${GERRIT_REFSPEC:-}"
 fi
-dpkg-buildpackage -uc -us -F
+
+if [[ $use_systemd == 0 ]]; then
+	dpkg-buildpackage -uc -us -F -R'debian/rules-nosystemd'
+else
+	dpkg-buildpackage -uc -us -F
+fi
 
 # Copy all the created files and clean up
 cp "$working_dir"/lizardfs?* "$output_dir"
