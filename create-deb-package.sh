cb9ef5e2f411a36b1d55a6351c98db5eb5d9965b
Piotr Sarna <sarna@skytechnology.pl>
2016-12-23 13:00:56 +0100

    Switch to unstable version 3.11.0-devel
    This commit comes with a switch to semantic versioning.
    
    Closes #462
    
    Change-Id: I435c44e7cdc062167e0ddf5c36c965d9caf71e90

--- create-deb-package.sh
+++ create-deb-package.sh
@@ -28,16 +28,7 @@ cd "$working_dir/lizardfs"
 # Move service files to debian/
 cp rpm/service-files/* debian/
 
-if [[ ${BUILD_NUMBER:-} && ${OFFICIAL_RELEASE:-} == "false" ]] ; then
-	# Jenkins has called us. Add build number to the package version
-	# and add information about commit to changelog.
-	lizard_version=$(dpkg-parsechangelog | awk '/^Version:/{print $2}')
-	version="${lizard_version}.${BUILD_NUMBER}"
-	dch -D "unstable" -v "$version" "Automatic jenkins build ${BUILD_URL:-}"
-	dch -D "unstable" -a "commit: ${GIT_COMMIT:-}"
-	dch -D "unstable" -a "refspec: ${GERRIT_REFSPEC:-}"
-fi
-
+sed -i '1 s/-devel//g' debian/changelog
 if [[ $use_systemd == 0 ]]; then
 	dpkg-buildpackage -uc -us -F -R'debian/rules-nosystemd'
 else
