fd3921d834a8248e2ce1398b5cd51ef97d4683ef
Hazeman <hazeman@skytechnology.pl>
2017-05-18 11:07:48 +0200

    cmake: Add missing license information
    This commit adds missing license information to cmake files.
    
    Change-Id: Ib9b9a39027548be371920a8cb0bb93163588f136

--- cmake/mingw32-w64-toolchain.cmake
+++ cmake/mingw32-w64-toolchain.cmake
@@ -1,3 +1,19 @@
+#  Copyright 2016-2017 Skytechnology sp. z o.o..
+#
+#  This file is part of LizardFS.
+#
+#  LizardFS is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, version 3.
+#
+#  LizardFS is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LizardFS  If not, see <http://www.gnu.org/licenses/>.
+
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_VERSION 8)
 
