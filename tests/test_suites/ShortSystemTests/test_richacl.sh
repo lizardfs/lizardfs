a1a69f53bc1bbcb1511e5e518d044c421c8f2ce8
Hazeman <hazeman@skytechnology.pl>
2017-08-25 16:40:41 +0200

    tests: Add test for RichACL
    This commit adds test using test suite from RichACL tools.
    
    Change-Id: Iad37bc568aaf94e17a659c441f9dbc30844dd1a7

---
+++ tests/test_suites/ShortSystemTests/test_richacl.sh
@@ -0,0 +1,16 @@
+timeout_set 70 seconds
+
+CHUNKSERVERS=3 \
+	USE_RAMDISK=YES \
+	MOUNT_EXTRA_CONFIG="mfscachemode=NEVER,enablefilelocks=1" \
+	CHUNKSERVER_EXTRA_CONFIG="READ_AHEAD_KB = 1024|MAX_READ_BEHIND_KB = 2048"
+	setup_local_empty_lizardfs info
+
+cd ${info[mount0]}
+
+git clone https://github.com/andreas-gruenbacher/richacl.git
+
+cd richacl
+./autogen.sh
+./configure
+make check
