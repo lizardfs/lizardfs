e517b69ca9b6b33493b58acff23a73e3b1af18e1
ppjski <przemyslaw.poljanski@lizardfs.com>
2020-02-20 12:47:01 +0100

    cgi: Fix bugs in CGI after upgrade to Python3
    None is replaced by LessThanComparableNone class's instance when
    used as an element of a tuple which has to be compared with other
    tuples when sorting them.
    This is because of a change in Python3 that None cannot be used in
    ordering comparison. HTML generation code in CGI project uses the fact
    that in Python2 None is considered less than any value of
    a non-NoneType type in ordering comparisons.
    LessThanComparableNone type has exactly this property. That is
    a special null-like value that can take part in ordering comparisons.
    
    Change-Id: I0e4bb4cb0707c95720ebf457c5e1d278c3e7b970

--- tests/test_suites/SanityChecks/test_cgi_using_pylint.sh
+++ tests/test_suites/SanityChecks/test_cgi_using_pylint.sh
@@ -1,7 +1,13 @@
-assert_program_installed pylint
+if is_program_installed pylint && pylint --version | grep -q "Python 3" ; then
+	pylintexec="pylint"
+elif is_program_installed pylint3 ; then
+	pylintexec="pylint3"
+else
+	test_fail "pylint is not installed"
+fi
 
 # Get paths to mfs.cgi, chart.cgi and the CGI server
 files=$(echo $LIZARDFS_ROOT/share/mfscgi/*.cgi $LIZARDFS_ROOT/sbin/lizardfs-cgiserver)
 
 # Validate all found files using pylint
-expect_empty "$(pylint -E $files || true)"
+expect_empty "$($pylintexec -E $files || true)"
