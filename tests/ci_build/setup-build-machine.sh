b40ff981e7de4c20a82961f7dca10f03b8ebfc6e
Baldor <baldor@lizardfs.org>
2022-12-13 01:55:30 +0100

    build: Add setup scripts for build and test machines

---
+++ tests/ci_build/setup-build-machine.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -eux -o pipefail
+script_dir="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
+
+die() {  echo "Error: ${*}" >&2;  exit 1; }
+extract_paragraphs() {
+	local search="${1}"
+	local file="${2}"
+	awk -v RS='' '/'"${search}"'/' "${file}"
+}
+
+apt-get install -y sudo
+
+setup_machine_script="${script_dir}/../setup_machine.sh"
+[ -f "${setup_machine_script}" ] || die "Script not found: ${setup_machine_script}"
+
+extract_paragraphs 'echo Install necessary programs' "${setup_machine_script}" | \
+	sed "s/apt-get install/apt-get install -y/g" | \
+	bash -x /dev/stdin
+
+# Extras
+apt-get install -y \
+  libdb-dev \
+  libjudy-dev
---
+++ tests/ci_build/setup-test-machine.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -eux -o pipefail
+script_dir="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
+
+die() {  echo "Error: ${*}" >&2;  exit 1; }
+
+# Install necessary packages
+apt-get install -y fuse libfuse-dev
+# necessary for debian
+apt-get install -y sudo valgrind git rsyslog psmisc
+apt-get install -y time bc
+apt-get install -y vim
+
+# Run LizardFS setup script
+mkdir -p /mnt/hdd_0 /mnt/hdd_1 /mnt/hdd_2 /mnt/hdd_3 /mnt/hdd_4
+setup_machine_script="${script_dir}/../setup_machine.sh"
+[ -f "${setup_machine_script}" ] || die "Script not found: ${setup_machine_script}"
+sed "s/apt-get install/apt-get install -y/g" "${setup_machine_script}" \
+	| grep -v '^[[:space:]]*mount[[:space:]]*[^[:space:]]*$' \
+	| bash -x /dev/stdin setup /mnt/hdd_0 /mnt/hdd_1 /mnt/hdd_2 /mnt/hdd_3 /mnt/hdd_4
+chown lizardfstest:lizardfstest /mnt/hdd_0 /mnt/hdd_1 /mnt/hdd_2 /mnt/hdd_3 /mnt/hdd_4
+
+# Extras
+apt-get install -y \
+  libdb-dev \
+  libjudy-dev \
+  pylint
+
+cp "${script_dir}/60-ip_port_range.conf" /etc/sysctl.d/
